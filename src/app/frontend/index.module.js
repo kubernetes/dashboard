eb12a53484a119fc286e166e43647a632a6b68eb
bryk <bryk@google.com>
2015-10-20 12:19:57 +0200

    Defie source code folder structure with sample app
    This is a sample Angular application written in ES6 with modules. It has
    sample unit and integration tests. It was generated with a help of
    Yeoman.
    
    Don't mind what the application does right now. I'll use it for a next
    few days to verify that build system is working. Then it will be
    revamped.

---
+++ src/app/frontend/index.module.js
@@ -0,0 +1,32 @@
+// Copyright 2015 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/**
+ * @fileoverview Entry point module to the application. Loads and configures other modules needed
+ * to bootstrap the application
+ */
+import { config } from './index.config';
+import { routerConfig, RouterController } from './index.route';
+import { runBlock } from './index.run';
+import { MainController } from './main/main.controller';
+
+export default angular.module(
+    'kubernetesConsole',
+    ['ngAnimate', 'ngSanitize', 'ngMessages', 'ngAria', 'ngResource', 'ngNewRouter', 'ngMaterial'])
+    .config(config)
+    .config(routerConfig)
+    .run(runBlock)
+    .controller('RouterController', RouterController)
+    .controller('MainController', MainController);
+
