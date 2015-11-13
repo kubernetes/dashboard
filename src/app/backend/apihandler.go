48cfc6c436154cbdbb593bef6a900aa7b7f69df3
bryk <bryk@google.com>
2015-11-13 12:23:14 +0100

    Backend implementation of the service list view
    1. This is a model for mocks:
    https://github.com/kubernetes/dashboard/blob/master/designs/11-11-2015/16_card%20variations.png
    2. Did little renaming (used the word "microservice") until we agree what the list exactly shows.
    3. The implementation is very basic and will evolve over time.

--- src/app/backend/apihandler.go
+++ src/app/backend/apihandler.go
@@ -22,35 +22,67 @@ import (
 )
 
 // Creates a new HTTP handler that handles all requests to the API of the backend.
-func CreateApiHandler(client *client.Client) http.Handler {
+func CreateHttpApiHandler(client *client.Client) http.Handler {
+	apiHandler := ApiHandler{client}
 	wsContainer := restful.NewContainer()
 
-	// TODO(bryk): This is for tests only. Replace with real implementation once ready.
-	ws := new(restful.WebService)
-	ws.Path("/api/deploy").
+	deployWs := new(restful.WebService)
+	deployWs.Path("/api/deploy").
 		Consumes(restful.MIME_JSON).
 		Produces(restful.MIME_JSON)
-	ws.Route(ws.POST("").To(func(request *restful.Request, response *restful.Response) {
-		cfg := new(DeployAppConfig)
-		if err := request.ReadEntity(cfg); err != nil {
-			HandleInternalError(response, err)
-			return
-		}
-		if err := DeployApp(cfg, client); err != nil {
-			HandleInternalError(response, err)
-			return
-		}
+	deployWs.Route(
+		deployWs.POST("").
+			To(apiHandler.handleDeploy).
+			Reads(DeployAppConfig{}).
+			Writes(DeployAppConfig{}))
+	wsContainer.Add(deployWs)
 
-		response.WriteHeaderAndEntity(http.StatusCreated, cfg)
-	}).Reads(DeployAppConfig{}).Writes(DeployAppConfig{}))
-
-	wsContainer.Add(ws)
+	microserviceListWs := new(restful.WebService)
+	microserviceListWs.Path("/api/microservice").
+		Produces(restful.MIME_JSON)
+	microserviceListWs.Route(
+		microserviceListWs.GET("").
+			To(apiHandler.handleGetMicroserviceList).
+			Writes(MicroserviceList{}))
+	wsContainer.Add(microserviceListWs)
 
 	return wsContainer
 }
 
+type ApiHandler struct {
+	client *client.Client
+}
+
+// Handles deploy API call.
+func (apiHandler *ApiHandler) handleDeploy(request *restful.Request, response *restful.Response) {
+	cfg := new(DeployAppConfig)
+	if err := request.ReadEntity(cfg); err != nil {
+		handleInternalError(response, err)
+		return
+	}
+	if err := DeployApp(cfg, apiHandler.client); err != nil {
+		handleInternalError(response, err)
+		return
+	}
+
+	response.WriteHeaderAndEntity(http.StatusCreated, cfg)
+}
+
+// Handles get microservice list API call.
+func (apiHandler *ApiHandler) handleGetMicroserviceList(
+	request *restful.Request, response *restful.Response) {
+
+	result, err := GetMicroserviceList(apiHandler.client)
+	if err != nil {
+		handleInternalError(response, err)
+		return
+	}
+
+	response.WriteHeaderAndEntity(http.StatusCreated, result)
+}
+
 // Handler that writes the given error to the response and sets appropriate HTTP status headers.
-func HandleInternalError(response *restful.Response, err error) {
+func handleInternalError(response *restful.Response, err error) {
 	response.AddHeader("Content-Type", "text/plain")
 	response.WriteErrorString(http.StatusInternalServerError, err.Error()+"\n")
 }
