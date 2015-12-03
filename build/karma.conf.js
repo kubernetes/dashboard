60f83e561d1fb93beac76ede47448f68ef90c903
bryk <bryk@google.com>
2015-12-03 14:07:10 +0100

    Updates to make code work with newest dependencies
    BREAKING CHANGE: Now node 4 is required to build the project. This is
    because protractor 3 requires is. I decided to make the switch now,
    because it would happen anyway at some point.

--- build/karma.conf.js
+++ build/karma.conf.js
@@ -1,13 +1,13 @@
 // Copyright 2015 Google Inc. All Rights Reserved.
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the 'License');
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an 'AS IS' BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
@@ -47,7 +47,7 @@ function getFileList() {
  *
  * @param {!Object} config
  */
-export default function(config) {
+module.exports = function(config) {
   let configuration = {
     basePath: conf.paths.base,
 
@@ -88,7 +88,7 @@ export default function(config) {
       paths: [conf.paths.frontendSrc, conf.paths.frontendTest],
       transform: [
         // Transform ES6 code into ES5 so that browsers can digest it.
-        'babelify',
+        ['babelify', {'presets': ['es2015']}],
       ],
     },
 
@@ -112,4 +112,4 @@ export default function(config) {
   configuration.preprocessors[path.join(conf.paths.frontendSrc, '**/*.html')] = ['ng-html2js'];
 
   config.set(configuration);
-}
+};
