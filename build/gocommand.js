7a28e0ffa57c9847d694fa30069a7731f426b167
Atanas Mirchev <atanas.mirchev@est.fujitsu.com>
2015-12-03 14:59:47 +0100

    Added go environment prerequisite checks, godep is now installed via npm

--- build/gocommand.js
+++ build/gocommand.js
@@ -17,38 +17,109 @@
  */
 import child from 'child_process';
 import lodash from 'lodash';
+import q from 'q';
 
 import conf from './conf';
 
+// Add base directory to the gopath so that local imports work.
+const sourceGopath = `${conf.paths.backendTmp}`;
+// Add the project's required go tools to the PATH.
+const devPath = `${process.env.PATH}:${conf.paths.goTools}/bin`;
+
 /**
- * Spawns Go process wrapped with the Godep command. Backend source files must me packaged with
- * 'package-backend-source' task before running this command.
+ * The environment needed for the execution of any go command.
+ */
+const env = lodash.merge(process.env, {GOPATH: sourceGopath, PATH: devPath});
+
+/**
+ * Spawns a Go process wrapped with the Godep command after making sure all GO prerequisites are
+ * present. Backend source files must be packaged with 'package-backend-source' task before running
+ * this command.
  *
- * @param {!Array<string>} args
- * @param {function(?Error=)} doneFn
+ * @param {!Array<string>} args - Arguments of the go command.
+ * @param {function(?Error=)} doneFn - Callback.
  */
 export default function spawnGoProcess(args, doneFn) {
-  // Add base directory to the gopath so that local imports work.
-  if (!process.env.GOPATH) {
-    doneFn(new Error('GOPATH is not set. Go process will not be spawned.'));
-    return;
-  }
+  checkPrerequisites().then(() => spawnProcess(args)).then(doneFn).fail((error) => doneFn(error));
+}
 
-  let sourceGopath = `${process.env.GOPATH}:${conf.paths.backendTmp}`;
-  let env = lodash.merge(process.env, {GOPATH: sourceGopath});
+/**
+ * Checks if all prerequisites for a go-command execution are present.
+ * @return {Q.Promise} A promise object.
+ */
+function checkPrerequisites() {
+  return checkGo().then(checkGodep);
+}
 
+/**
+ * Checks if go is on the PATH prior to a go command execution, promises an error otherwise.
+ * @return {Q.Promise} A promise object.
+ */
+function checkGo() {
+  let deferred = q.defer();
+  child.exec(
+      'which go',
+      {
+        env: env,
+      },
+      function(error, stdout, stderror) {
+        if (error || stderror || !stdout) {
+          deferred.reject(
+              new Error(
+                  'Go is not on the path. Please pass the PATH variable when you run ' +
+                  'the gulp task with "PATH=$PATH" or install go if you have not yet.'));
+          return;
+        }
+        deferred.resolve();
+      });
+  return deferred.promise;
+}
+
+/**
+ * Checks if godep is on the PATH prior to a go command execution, promises an error otherwise.
+ * @return {Q.Promise} A promise object.
+ */
+function checkGodep() {
+  let deferred = q.defer();
+  child.exec(
+      'which godep',
+      {
+        env: env,
+      },
+      function(error, stdout, stderror) {
+        if (error || stderror || !stdout) {
+          deferred.reject(
+              new Error(
+                  'Godep is not on the path. ' +
+                  'Please run "npm install" in the base directory of the project.'));
+          return;
+        }
+        deferred.resolve();
+      });
+  return deferred.promise;
+}
+
+/**
+ * Spawns Go process wrapped with the Godep command.
+ * Promises an error if the go command process fails.
+ *
+ * @param {!Array<string>} args - Arguments of the go command.
+ * @return {Q.Promise} A promise object.
+ */
+function spawnProcess(args) {
+  let deferred = q.defer();
   let goTask = child.spawn('godep', ['go'].concat(args), {
     env: env,
     stdio: 'inherit',
   });
-
   // Call Gulp callback on task exit. This has to be done to make Gulp dependency management
   // work.
   goTask.on('exit', function(code) {
-    if (code === 0) {
-      doneFn();
-    } else {
-      doneFn(new Error(`Go command error, code: ${code}`));
+    if (code !== 0) {
+      deferred.reject(Error(`Go command error, code: ${code}`));
+      return;
     }
+    deferred.resolve();
   });
+  return deferred.promise;
 }
