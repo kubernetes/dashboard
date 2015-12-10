24823531bfff3be6bc701bde2b6dfdfbf8323bf7
Christoph Held <christoph.held@est.fujitsu.com>
2015-12-10 15:14:37 +0100

    simple error dialog

---
+++ src/app/frontend/common/errorhandling/errordialog_service.js
@@ -0,0 +1,42 @@
+// Copyright 2015 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/**
+ * Error dialog to display errors which could not be responded properly
+ *
+ * @final
+ */
+export default class ErrorDialog {
+  /**
+   * @param {!md.$dialog} $mdDialog
+   * @ngInject
+   */
+  constructor($mdDialog) {
+    /** @private {!md.$dialog} */
+    this.mdDialog_ = $mdDialog;
+  }
+
+  /**
+   * Opens a pop-up window that displays the error message
+   *
+   * @param {string} message
+   * @export
+   */
+  open(message) {
+    let alert = this.mdDialog_.alert();
+    alert.title(message);
+    alert.ok('Close');
+    this.mdDialog_.show(alert);
+  }
+}
