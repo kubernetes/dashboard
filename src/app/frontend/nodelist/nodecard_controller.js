0f9d9c88ac93d5243febb023eca9acf7ed00e3ff
Hannes Lohmander <hannes@lohmander.me>
2016-01-09 21:46:55 +0100

    Add code docs to nodecard controller and clear interval on scope $destroy

--- src/app/frontend/nodelist/nodecard_controller.js
+++ src/app/frontend/nodelist/nodecard_controller.js
@@ -1,22 +1,45 @@
-export default class NodeListCardController {
-    constructor($state, $scope, nodeStatsService) {
+/**
+* Controller for the Node card
+*/
+export default class NodeCardController {
+    /**
+    * @param {$scope} Angular scope
+    * @param {!./nodestats_service.NodeStatsService} nodeStatsService
+    */
+    constructor($scope, nodeStatsService) {
+        /**
+        * Initialized from the scope
+        * @export {!backendApi.Node}
+        */
         this.node;
 
-        this._state = $state;
+        /**
+        * @private {!$scope}
+        */
         this._scope = $scope;
+
+        /**
+        * @private {!./nodestats_service.NodeStatsService}
+        */
         this._nodeService = nodeStatsService;
 
+        // bind to self
         this.updateData = this.updateData.bind(this);
         this.fetchNodeStatData = this.fetchNodeStatData.bind(this);
 
+        // initialize chart options
         this.initChartOptions();
 
+        // only try to get node stats if it's ready
         if (this.node.status === 'Ready') {
             this.fetchNodeStatData();
             this.pollNodeStatData();
         }
     }
 
+    /**
+    * Initializes all chart options
+    */
     initChartOptions() {
         this._scope.series = ['CPU (%)', 'Memory (%)'];
         this._scope.colors = ['#326DE6', '#e5be32'];
@@ -26,16 +49,29 @@ export default class NodeListCardController {
         };
     }
 
+    /**
+    * Polls the node stats endpoint indefinitely every other second
+    */
     pollNodeStatData() {
-        setInterval(this.fetchNodeStatData, 2000);
+        const pollInterval = setInterval(this.fetchNodeStatData, 2000);
+        this._scope.$on('$destroy', () => {
+            clearInterval(pollInterval);
+        });
     }
 
+    /**
+    * Fetch node stat data
+    */
     fetchNodeStatData() {
         this._nodeService
             .getNodeStats(this.node.name)
             .then(this.updateData);
     }
 
+    /**
+    * Update chart data
+    * @param {!backendApi.NodeStats} data
+    */
     updateData(data) {
         let cpu = [];
         let mem = [];
@@ -49,6 +85,10 @@ export default class NodeListCardController {
         this._scope.labels = Array.apply(null, Array(cpu.length)).map(() => '');
     }
 
+    /**
+    * @return {string}
+    * @export
+    */
     getFormattedLabels() {
         return Object.keys(this.node.labels).map(key => {
             return `${key}: ${this.node.labels[key]}`;
