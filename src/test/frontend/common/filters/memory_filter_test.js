eb50b55049450d709624d3f23660d62316198a38
bryk <bryk@google.com>
2016-01-26 13:27:47 +0100

    Display metrics usage and connect to in-cluster heapster
    TODO that is left: format CPU usage. Now raw number is displayed.

---
+++ src/test/frontend/common/filters/memory_filter_test.js
@@ -0,0 +1,41 @@
+// Copyright 2015 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import filtersModule from 'common/filters/filters_module';
+
+describe('Memory filter', () => {
+  /** @type {function(!number):string} */
+  let memoryFilter;
+
+  beforeEach(() => {
+    angular.mock.module(filtersModule.name);
+    angular.mock.inject((_kdMemoryFilter_) => { memoryFilter = _kdMemoryFilter_; });
+  });
+
+  it('should format memory', () => {
+    expect(memoryFilter(0)).toEqual('0 B');
+    expect(memoryFilter(1)).toEqual('1 B');
+    expect(memoryFilter(2)).toEqual('2 B');
+    expect(memoryFilter(1000)).toEqual('1,000 B');
+    expect(memoryFilter(1024)).toEqual('1,024 B');
+    expect(memoryFilter(1025)).toEqual('1.001 KiB');
+    expect(memoryFilter(7896)).toEqual('7.711 KiB');
+    expect(memoryFilter(109809)).toEqual('107.235 KiB');
+    expect(memoryFilter(768689899)).toEqual('733.080 MiB');
+    expect(memoryFilter(768689899789)).toEqual('715.898 GiB');
+    expect(memoryFilter(76868989978978)).toEqual('69.912 TiB');
+    expect(memoryFilter(7686898997897878)).toEqual('6.827 PiB');
+    expect(memoryFilter(768689899789787867898766)).toEqual('682,733,780.435 PiB');
+  });
+});
