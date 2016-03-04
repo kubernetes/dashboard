b546ce39b1c30506b56a9a4ae8b01b09035d247d
Denis Poisson <Denis.Poisson@ts.fujitsu.com>
2016-03-04 09:43:42 +0100

    Validation on Deploy Page: Labels -> Label Key(validation for 'value' will be in a separate PR).

--- src/app/frontend/deploy/deploylabel_controller.js
+++ src/app/frontend/deploy/deploylabel_controller.js
@@ -88,22 +88,44 @@ export default class DeployLabelController {
   addNewLabel_() { this.labels.push(new DeployLabel()); }
 
   /**
-   * Validates label withing label form.
+   * Validates label within label form.
    * Current checks:
    *  - duplicated key
+   *  - key prefix pattern
+   *  - key name pattern
+   *  - key prefix length
+   *  - key name length
    * @param {!angular.FormController|undefined} labelForm
    * @private
    */
+  // TODO: @digitalfishpond Move these validations to directives
   validateKey_(labelForm) {
     if (angular.isDefined(labelForm)) {
       /** @type {!angular.NgModelController} */
       let elem = labelForm.key;
-
-      // TODO(floreks): Validate label key/value.
+      /** @type {!RegExp} */
+      let PrefixPattern = /^(.*\/.*)$/;
       /** @type {boolean} */
-      let isValid = !this.isDuplicated_();
+      let isPrefixed = PrefixPattern.test(this.label.key);
+      /** @type {number} */
+      let slashPosition = isPrefixed ? this.label.key.indexOf("/") : -1;
 
-      elem.$setValidity('unique', isValid);
+      /** @type {boolean} */
+      let isUnique = !this.isKeyDuplicated_();
+      /** @type {boolean} */
+      let isKeyPrefixPatternOk = this.matchesKeyPrefixPattern_(isPrefixed, slashPosition);
+      /** @type {boolean} */
+      let isKeyNamePatternOk = this.matchesKeyNamePattern_(isPrefixed, slashPosition);
+      /** @type {boolean} */
+      let isKeyPrefixLengthOk = this.matchesKeyPrefixLength_(isPrefixed, slashPosition);
+      /** @type {boolean} */
+      let isKeyNameLengthOk = this.matchesKeyNameLength_(isPrefixed, slashPosition);
+
+      elem.$setValidity('unique', isUnique);
+      elem.$setValidity('prefixPattern', isKeyPrefixPatternOk);
+      elem.$setValidity('namePattern', isKeyNamePatternOk);
+      elem.$setValidity('prefixLength', isKeyPrefixLengthOk);
+      elem.$setValidity('nameLength', isKeyNameLengthOk);
     }
   }
 
@@ -113,7 +135,7 @@ export default class DeployLabelController {
    * @return {boolean}
    * @private
    */
-  isDuplicated_() {
+  isKeyDuplicated_() {
     /** @type {number} */
     let duplications = 0;
 
@@ -124,6 +146,84 @@ export default class DeployLabelController {
     });
 
     return duplications > 1;
+  }
+
+  /**
+   * Returns true if the label key prefix (before the "/" if there is one) matches a lowercase
+   * alphanumeric character
+   * optionally followed by lowercase alphanumeric or '-' or '.' and ending with a lower case
+   * alphanumeric character,
+   * with '.' only permitted if surrounded by lowercase alphanumeric characters (eg:
+   * 'good.prefix-pattern',
+   * otherwise returns false.
+   * @return {boolean}
+   * @param {boolean} isPrefixed
+   * @param {number} slashPosition
+   * @private
+   */
+  matchesKeyPrefixPattern_(isPrefixed, slashPosition) {
+    /** @type {!RegExp} */
+    let labelKeyPrefixPattern = /^[a-z0-9]([-a-z0-9]*[a-z0-9])?(\.[a-z0-9]([-a-z0-9]*[a-z0-9])?)*$/;
+    /** @type {string} */
+    let labelPrefix = isPrefixed ? this.label.key.substring(0, slashPosition) : "valid-pattern";
+
+    return (labelKeyPrefixPattern.test(labelPrefix));
+  }
+
+  /**
+   * Returns true if the label key name (after the "/" if there is one) matches an alphanumeric
+   * character (upper
+   * or lower case) optionally followed by alphanumeric or -_. and ending with an alphanumeric
+   * character
+   * (upper or lower case), otherwise returns false.
+   * @return {boolean}
+   * @param {boolean} isPrefixed
+   * @param {number} slashPosition
+   * @private
+   */
+  matchesKeyNamePattern_(isPrefixed, slashPosition) {
+    /** @type {!RegExp} */
+    let labelKeyNamePattern = /^([A-Za-z0-9][-A-Za-z0-9_.]*)?[A-Za-z0-9]$/;
+    /** @type {string} */
+    let labelName = isPrefixed ? this.label.key.substring(slashPosition + 1) : this.label.key;
+
+    return (labelKeyNamePattern.test(labelName));
+  }
+
+  /**
+   * Returns true if the label key name (after the "/" if there is one) is equal or shorter than 253
+   * characters,
+   * otherwise returns false.
+   * @return {boolean}
+   * @param {boolean} isPrefixed
+   * @param {number} slashPosition
+   * @private
+   */
+  matchesKeyPrefixLength_(isPrefixed, slashPosition) {
+    /** @type {number} */
+    let maxLength = 253;
+    /** @type {string} */
+    let labelPrefix = isPrefixed ? this.label.key.substring(0, slashPosition) : '';
+
+    return (labelPrefix.length <= maxLength);
+  }
+
+  /**
+   * Returns true if the label key name (after the "/" if there is one) is equal or shorter than 63
+   * characters,
+   * otherwise returns false.
+   * @return {boolean}
+   * @param {boolean} isPrefixed
+   * @param {number} slashPosition
+   * @private
+   */
+  matchesKeyNameLength_(isPrefixed, slashPosition) {
+    /** @type {number} */
+    let maxLength = 63;
+    /** @type {string} */
+    let labelName = isPrefixed ? this.label.key.substring(slashPosition + 1) : this.label.key;
+
+    return (labelName.length <= maxLength);
   }
 
   /**
