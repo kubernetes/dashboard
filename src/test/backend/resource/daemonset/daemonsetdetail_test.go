3eaf05c68fa593c4c0df37b86b8730a838d08467
Sebastien Coavoux <Sebastien.Coavoux@nuance.com>
2016-03-09 15:58:34 -0500

    Add: Test for new routes

---
+++ src/test/backend/resource/daemonset/daemonsetdetail_test.go
@@ -0,0 +1,284 @@
+// Copyright 2015 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package daemonset
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/kubernetes/dashboard/resource/common"
+	"k8s.io/kubernetes/pkg/api"
+	"k8s.io/kubernetes/pkg/api/unversioned"
+	"k8s.io/kubernetes/pkg/apis/extensions"
+	"k8s.io/kubernetes/pkg/client/unversioned/testclient"
+)
+
+func TestDeleteDaemonSetServices(t *testing.T) {
+	cases := []struct {
+		namespace, name string
+		DaemonSet       *extensions.DaemonSet
+		DaemonSetList   *extensions.DaemonSetList
+		serviceList     *api.ServiceList
+		expectedActions []string
+	}{
+		{
+			"test-namespace", "test-name",
+			&extensions.DaemonSet{
+				Spec: extensions.DaemonSetSpec{
+					Selector: &unversioned.LabelSelector{
+						MatchLabels: map[string]string{"app": "test"},
+					},
+				},
+			},
+			&extensions.DaemonSetList{
+				Items: []extensions.DaemonSet{
+					{Spec: extensions.DaemonSetSpec{
+						Selector: &unversioned.LabelSelector{
+							MatchLabels: map[string]string{"app": "test"},
+						},
+					}},
+				},
+			},
+			&api.ServiceList{
+				Items: []api.Service{
+					{Spec: api.ServiceSpec{Selector: map[string]string{"app": "test"}}},
+				},
+			},
+			[]string{"get", "list", "list", "delete"},
+		},
+		{
+			"test-namespace", "test-name",
+			&extensions.DaemonSet{
+				Spec: extensions.DaemonSetSpec{
+					Selector: &unversioned.LabelSelector{
+						MatchLabels: map[string]string{"app": "test"},
+					},
+				}},
+			&extensions.DaemonSetList{
+				Items: []extensions.DaemonSet{
+					{Spec: extensions.DaemonSetSpec{
+						Selector: &unversioned.LabelSelector{
+							MatchLabels: map[string]string{"app": "test"},
+						},
+					}},
+					{Spec: extensions.DaemonSetSpec{
+						Selector: &unversioned.LabelSelector{
+							MatchLabels: map[string]string{"app": "test"},
+						},
+					}},
+				},
+			},
+			&api.ServiceList{
+				Items: []api.Service{
+					{Spec: api.ServiceSpec{Selector: map[string]string{"app": "test"}}},
+				},
+			},
+			[]string{"get", "list"},
+		},
+		{
+			"test-namespace", "test-name",
+			&extensions.DaemonSet{
+				Spec: extensions.DaemonSetSpec{
+					Selector: &unversioned.LabelSelector{
+						MatchLabels: map[string]string{"app": "test"},
+					},
+				}},
+			&extensions.DaemonSetList{
+				Items: []extensions.DaemonSet{
+					{Spec: extensions.DaemonSetSpec{
+						Selector: &unversioned.LabelSelector{
+							MatchLabels: map[string]string{"app": "test"},
+						},
+					}},
+				},
+			},
+			&api.ServiceList{},
+			[]string{"get", "list", "list"},
+		},
+	}
+
+	for _, c := range cases {
+		fakeClient := testclient.NewSimpleFake(c.DaemonSet,
+			c.DaemonSetList, c.serviceList)
+
+		DeleteDaemonSetServices(fakeClient, c.namespace, c.name)
+
+		actions := fakeClient.Actions()
+		if len(actions) != len(c.expectedActions) {
+			t.Errorf("Unexpected actions: %v, expected %d actions got %d", actions,
+				len(c.expectedActions), len(actions))
+			continue
+		}
+
+		for i, verb := range c.expectedActions {
+			if actions[i].GetVerb() != verb {
+				t.Errorf("Unexpected action: %+v, expected %s",
+					actions[i], verb)
+			}
+		}
+	}
+}
+
+func TestFilterDaemonSetPodsforDS(t *testing.T) {
+
+	firstLabelSelectorMap := make(map[string]string)
+	firstLabelSelectorMap["name"] = "app-name-first"
+	firstLabelSelector := unversioned.LabelSelector{
+		MatchLabels: firstLabelSelectorMap,
+	}
+	secondLabelSelectorMap := make(map[string]string)
+	secondLabelSelectorMap["name"] = "app-name-second"
+
+	cases := []struct {
+		DaemonSet extensions.DaemonSet
+		pods      []api.Pod
+		expected  []api.Pod
+	}{
+		{
+			extensions.DaemonSet{
+				Spec: extensions.DaemonSetSpec{
+					Selector: &firstLabelSelector,
+				},
+			},
+			[]api.Pod{
+				{
+					ObjectMeta: api.ObjectMeta{
+						Name:   "first-pod-ok",
+						Labels: firstLabelSelectorMap,
+					},
+				},
+				{
+					ObjectMeta: api.ObjectMeta{
+						Name:   "second-pod-ok",
+						Labels: firstLabelSelectorMap,
+					},
+				},
+				{
+					ObjectMeta: api.ObjectMeta{
+						Name:   "third-pod-wrong",
+						Labels: secondLabelSelectorMap,
+					},
+				},
+			},
+			[]api.Pod{
+				{
+					ObjectMeta: api.ObjectMeta{
+						Name:   "first-pod-ok",
+						Labels: firstLabelSelectorMap,
+					},
+				},
+				{
+					ObjectMeta: api.ObjectMeta{
+						Name:   "second-pod-ok",
+						Labels: firstLabelSelectorMap,
+					},
+				},
+			},
+		},
+	}
+	for _, c := range cases {
+		actual := filterDaemonSetPods(c.DaemonSet, c.pods)
+		if !reflect.DeepEqual(actual, c.expected) {
+			t.Errorf("getExternalEndpoints(%+v, %+v) == %+v, expected %+v",
+				c.DaemonSet, c.pods, actual, c.expected)
+		}
+	}
+}
+
+func TestGetExternalEndpoints(t *testing.T) {
+	labelSelectorMap := make(map[string]string)
+	labelSelectorMap["name"] = "app-name"
+	labelSelector := unversioned.LabelSelector{
+		MatchLabels: labelSelectorMap,
+	}
+	cases := []struct {
+		DaemonSet extensions.DaemonSet
+		pods      []api.Pod
+		service   api.Service
+		nodes     []api.Node
+		expected  []common.Endpoint
+	}{
+		{
+			extensions.DaemonSet{
+				Spec: extensions.DaemonSetSpec{
+					Selector: &labelSelector,
+				},
+			},
+			[]api.Pod{
+				{
+					Spec: api.PodSpec{
+						NodeName: "node",
+					},
+					ObjectMeta: api.ObjectMeta{
+						Labels: labelSelectorMap,
+					},
+				},
+			},
+			api.Service{
+				Spec: api.ServiceSpec{
+					Type: api.ServiceTypeNodePort,
+					Ports: []api.ServicePort{
+						{
+							Protocol: "TCP",
+							NodePort: 30100,
+						},
+						{
+							Protocol: "TCP",
+							NodePort: 30101,
+						},
+					},
+				},
+			},
+			[]api.Node{{
+				ObjectMeta: api.ObjectMeta{
+					Name: "node",
+				},
+				Status: api.NodeStatus{
+					Addresses: []api.NodeAddress{
+						{
+							Type:    api.NodeExternalIP,
+							Address: "192.168.1.108",
+						},
+					},
+				},
+			}},
+			[]common.Endpoint{
+				{
+					Host: "192.168.1.108",
+					Ports: []common.ServicePort{
+						{
+							Port: 30100, Protocol: "TCP",
+						},
+					},
+				},
+				{
+					Host: "192.168.1.108",
+					Ports: []common.ServicePort{
+						{
+							Port: 30101, Protocol: "TCP",
+						},
+					},
+				},
+			},
+		},
+	}
+	for _, c := range cases {
+		actual := getExternalEndpoints(c.DaemonSet, c.pods, c.service, c.nodes)
+		if !reflect.DeepEqual(actual, c.expected) {
+			t.Errorf("getExternalEndpoints(%+v, %+v, %+v, %+v) == %+v, expected %+v",
+				c.DaemonSet, c.pods, c.service, c.nodes, actual, c.expected)
+		}
+	}
+}
---
+++ src/test/backend/resource/daemonset/daemonsetlist_test.go
@@ -0,0 +1,242 @@
+// Copyright 2015 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package daemonset
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/kubernetes/dashboard/resource/common"
+	"github.com/kubernetes/dashboard/resource/event"
+	"k8s.io/kubernetes/pkg/api"
+	"k8s.io/kubernetes/pkg/api/unversioned"
+	"k8s.io/kubernetes/pkg/apis/extensions"
+)
+
+func TestGetMatchingServicesforDS(t *testing.T) {
+	cases := []struct {
+		services  []api.Service
+		DaemonSet *extensions.DaemonSet
+		expected  []api.Service
+	}{
+		{nil, nil, nil},
+		{
+			[]api.Service{{Spec: api.ServiceSpec{Selector: map[string]string{"app": "my-name"}}}},
+			&extensions.DaemonSet{
+				Spec: extensions.DaemonSetSpec{
+					Selector: &unversioned.LabelSelector{
+						MatchLabels: map[string]string{"app": "my-name"},
+					},
+				},
+			},
+			[]api.Service{{Spec: api.ServiceSpec{Selector: map[string]string{"app": "my-name"}}}},
+		},
+		{
+			[]api.Service{
+				{Spec: api.ServiceSpec{Selector: map[string]string{"app": "my-name"}}},
+				{Spec: api.ServiceSpec{Selector: map[string]string{"app": "my-name", "ver": "2"}}},
+			},
+			&extensions.DaemonSet{
+				Spec: extensions.DaemonSetSpec{
+					Selector: &unversioned.LabelSelector{
+						MatchLabels: map[string]string{"app": "my-name"},
+					},
+				},
+			},
+			[]api.Service{{Spec: api.ServiceSpec{Selector: map[string]string{"app": "my-name"}}}},
+		},
+	}
+	for _, c := range cases {
+		actual := getMatchingServicesforDS(c.services, c.DaemonSet)
+		if !reflect.DeepEqual(actual, c.expected) {
+			t.Errorf("getMatchingServices(%+v, %+v) == %+v, expected %+v",
+				c.services, c.DaemonSet, actual, c.expected)
+		}
+	}
+}
+
+func TestGetDaemonSetList(t *testing.T) {
+	events := []api.Event{}
+
+	cases := []struct {
+		daemonSets []extensions.DaemonSet
+		services   []api.Service
+		pods       []api.Pod
+		nodes      []api.Node
+		expected   *DaemonSetList
+	}{
+		{nil, nil, nil, nil, &DaemonSetList{DaemonSets: []DaemonSet{}}},
+		{
+			[]extensions.DaemonSet{
+				{
+					ObjectMeta: api.ObjectMeta{
+						Name:      "my-app-1",
+						Namespace: "namespace-1",
+					},
+					Spec: extensions.DaemonSetSpec{
+						Selector: &unversioned.LabelSelector{
+							MatchLabels: map[string]string{"app": "my-name-1"},
+						},
+						Template: api.PodTemplateSpec{
+							Spec: api.PodSpec{Containers: []api.Container{{Image: "my-container-image-1"}}},
+						},
+					},
+				},
+				{
+					ObjectMeta: api.ObjectMeta{
+						Name:      "my-app-2",
+						Namespace: "namespace-2",
+					},
+					Spec: extensions.DaemonSetSpec{
+						Selector: &unversioned.LabelSelector{
+							MatchLabels: map[string]string{"app": "my-name-2", "ver": "2"},
+						},
+						Template: api.PodTemplateSpec{
+							Spec: api.PodSpec{Containers: []api.Container{{Image: "my-container-image-2"}}},
+						},
+					},
+				},
+			},
+			[]api.Service{
+				{
+					Spec: api.ServiceSpec{Selector: map[string]string{"app": "my-name-1"}},
+					ObjectMeta: api.ObjectMeta{
+						Name:      "my-app-1",
+						Namespace: "namespace-1",
+					},
+				},
+				{
+					Spec: api.ServiceSpec{Selector: map[string]string{"app": "my-name-2", "ver": "2"}},
+					ObjectMeta: api.ObjectMeta{
+						Name:      "my-app-2",
+						Namespace: "namespace-2",
+					},
+				},
+			},
+			[]api.Pod{
+				{
+					ObjectMeta: api.ObjectMeta{
+						Namespace: "namespace-1",
+						Labels:    map[string]string{"app": "my-name-1"},
+					},
+					Status: api.PodStatus{
+						Phase: api.PodFailed,
+					},
+				},
+				{
+					ObjectMeta: api.ObjectMeta{
+						Namespace: "namespace-1",
+						Labels:    map[string]string{"app": "my-name-1"},
+					},
+					Status: api.PodStatus{
+						Phase: api.PodFailed,
+					},
+				},
+				{
+					ObjectMeta: api.ObjectMeta{
+						Namespace: "namespace-1",
+						Labels:    map[string]string{"app": "my-name-1"},
+					},
+					Status: api.PodStatus{
+						Phase: api.PodPending,
+					},
+				},
+				{
+					ObjectMeta: api.ObjectMeta{
+						Namespace: "namespace-2",
+						Labels:    map[string]string{"app": "my-name-1"},
+					},
+					Status: api.PodStatus{
+						Phase: api.PodPending,
+					},
+				},
+				{
+					ObjectMeta: api.ObjectMeta{
+						Namespace: "namespace-1",
+						Labels:    map[string]string{"app": "my-name-1"},
+					},
+					Status: api.PodStatus{
+						Phase: api.PodRunning,
+					},
+				},
+				{
+					ObjectMeta: api.ObjectMeta{
+						Namespace: "namespace-1",
+						Labels:    map[string]string{"app": "my-name-1"},
+					},
+					Status: api.PodStatus{
+						Phase: api.PodSucceeded,
+					},
+				},
+				{
+					ObjectMeta: api.ObjectMeta{
+						Namespace: "namespace-1",
+						Labels:    map[string]string{"app": "my-name-1"},
+					},
+					Status: api.PodStatus{
+						Phase: api.PodUnknown,
+					},
+				},
+			},
+			[]api.Node{{
+				Status: api.NodeStatus{
+					Addresses: []api.NodeAddress{
+						{
+							Type:    api.NodeExternalIP,
+							Address: "192.168.1.108",
+						},
+					},
+				},
+			},
+			},
+			&DaemonSetList{
+				DaemonSets: []DaemonSet{
+					{
+						ObjectMeta: common.ObjectMeta{
+							Name:      "my-app-1",
+							Namespace: "namespace-1",
+						},
+						ContainerImages:   []string{"my-container-image-1"},
+						InternalEndpoints: []common.Endpoint{{Host: "my-app-1.namespace-1"}},
+						Pods: common.PodInfo{
+							Failed:   2,
+							Pending:  1,
+							Running:  1,
+							Warnings: []event.Event{},
+						},
+					}, {
+						ObjectMeta: common.ObjectMeta{
+							Name:      "my-app-2",
+							Namespace: "namespace-2",
+						},
+						ContainerImages:   []string{"my-container-image-2"},
+						InternalEndpoints: []common.Endpoint{{Host: "my-app-2.namespace-2"}},
+						Pods: common.PodInfo{
+							Warnings: []event.Event{},
+						},
+					},
+				},
+			},
+		},
+	}
+	for _, c := range cases {
+		actual := getDaemonSetList(c.daemonSets, c.services, c.pods,
+			events, c.nodes)
+		if !reflect.DeepEqual(actual, c.expected) {
+			t.Errorf("getDaemonSetList(%#v, %#v) == \n%#v\nexpected \n%#v\n",
+				c.daemonSets, c.services, actual, c.expected)
+		}
+	}
+}
