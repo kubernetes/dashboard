c4b0ec431c4edd7e436454b32dc49fd808b7708c
Antoine Legrand <antoine.legrand@arkena.com>
2016-03-21 00:45:53 +0100

    Namespace

--- src/app/frontend/deploy/deployfromkpm_controller.js
+++ src/app/frontend/deploy/deployfromkpm_controller.js
@@ -1,3 +1,5 @@
+import showNamespaceDialog from './createnamespace_dialog';
+
 /**
  * Controller for the deploy from kpm directive.
  *
@@ -13,16 +15,19 @@ export default class DeployFromKpmController {
    * @ngInject
    */
   constructor($http, $scope, $log, $state, $resource, $q, $mdDialog) {
-    // Dependencies
-    this._http = $http;
-    this._state = $state;
-    this._q = $q;
+      // Dependencies
+      this._http = $http;
+      this._state = $state;
+      this._q = $q;
 
-    this.packageName = '';
-    this.namespace = '';
-    this.kpmBackendUrl = 'http://localhost:5000/api/v1/';
-    this.deployStatus = 'none';
-    this.resources = [];
+      this.namespaces;
+      this.packageName = '';
+      this.namespace = this.namespaces[0];
+
+      this.kpmBackendUrl = 'http://localhost:5000/api/v1/';
+      this.deployStatus = 'none';
+      this.resources = [];
+      this.mdDialog_ = $mdDialog;
   }
 
   /**
@@ -38,6 +43,32 @@ export default class DeployFromKpmController {
    */
   remove() {
     return this.performQuery('DELETE');
+  }
+
+  /**
+   * Displays new namespace creation dialog.
+   *
+   * @param {!angular.Scope.Event} event
+   * @export
+   */
+  handleNamespaceDialog(event) {
+    showNamespaceDialog(this.mdDialog_, event, this.namespaces)
+        .then(
+            /**
+             * Handles namespace dialog result. If namespace was created successfully then it
+             * will be selected, otherwise first namespace will be selected.
+             *
+             * @param {string|undefined} answer
+             */
+            (answer) => {
+              if (answer) {
+                this.namespace = answer;
+                this.namespaces = this.namespaces.concat(answer);
+              } else {
+                this.namespace = this.namespaces[0];
+              }
+            },
+            () => { this.namespace = this.namespaces[0]; });
   }
 
   performQuery(method) {
@@ -74,7 +105,7 @@ export default class DeployFromKpmController {
     var deferred = this._q.defer();
     this._http({
       method: 'GET',
-      url: 'https://api.kpm.sh/packages.json'
+      url: 'https://api.kpm.sh/api/v1/packages.json'
     })
     .success(function(data) {
       // FIXME: perform filtering server-side
