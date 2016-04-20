2880ca0ca1a1b78fc3be80649b208f3a14bcb64b
Christoph Held <christoph.held@est.fujitsu.com>
2016-04-20 11:45:14 +0200

    implemented validation for expose service

--- src/app/frontend/deploy/deployfromsettings_controller.js
+++ src/app/frontend/deploy/deployfromsettings_controller.js
@@ -173,6 +173,8 @@ export default class DeployFromSettingsController {
 
     /** @private {!md.$dialog} */
     this.mdDialog_ = $mdDialog;
+
+    $scope.$watch('ctrl.portMappings', () => { this.validateExposeService(); }, true);
   }
 
   /**
@@ -368,4 +370,13 @@ export default class DeployFromSettingsController {
    * @export
    */
   switchMoreOptions() { this.detail.showMoreOptions_ = !this.detail.showMoreOptions_; }
+
+  validateExposeService() {
+    let noPortMapping =
+        (this.portMappings !== undefined &&
+         this.portMappings.filter(this.isPortMappingFilled_).length === 0);
+    let isValid = !(this.isExternal && noPortMapping);
+
+    this.form['exposeService'].$setValidity('portMappingRequired', isValid);
+  }
 }
