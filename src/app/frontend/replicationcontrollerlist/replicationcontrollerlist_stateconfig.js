014905b97fe74b09b941f7829056d8c89b3851a4
Piotr Bryk <bryk@google.com>
2016-04-21 08:33:28 +0200

    Modernized RC list page
    What I did here:
    * I copied old RC list code
    * Changed all directives to angular 1.5 components
    * Removed custom CSS styles
    * Added missing tests
    
    What is missing:
    * Zerostate page
    * responsiveness (this is a problem of the common component)
    * Documentation

---
+++ src/app/frontend/replicationcontrollerlist/replicationcontrollerlist_stateconfig.js
@@ -0,0 +1,61 @@
+// Copyright 2015 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import {actionbarViewName} from 'chrome/chrome_state';
+import {stateName, stateUrl} from './replicationcontrollerlist_state';
+import {ReplicationControllerListController} from './replicationcontrollerlist_controller';
+import ReplicationControllerListActionBarController from './replicationcontrollerlistactionbar_controller';
+
+/**
+ * Configures states for the service view.
+ *
+ * @param {!ui.router.$stateProvider} $stateProvider
+ * @ngInject
+ */
+export default function stateConfig($stateProvider) {
+  $stateProvider.state(stateName, {
+    url: stateUrl,
+    resolve: {
+      'replicationControllers': resolveReplicationControllers,
+    },
+    data: {
+      'kdBreadcrumbs': {
+        'label': 'Replication Controllers',
+      },
+    },
+    views: {
+      '': {
+        controller: ReplicationControllerListController,
+        controllerAs: '$ctrl',
+        templateUrl: 'replicationcontrollerlist/replicationcontrollerlist.html',
+      },
+      [actionbarViewName]: {
+        controller: ReplicationControllerListActionBarController,
+        controllerAs: 'ctrl',
+        templateUrl: 'replicationcontrollerlist/replicationcontrollerlistactionbar.html',
+      },
+    },
+  });
+}
+
+/**
+ * @param {!angular.$resource} $resource
+ * @return {!angular.$q.Promise}
+ * @ngInject
+ */
+export function resolveReplicationControllers($resource) {
+  /** @type {!angular.Resource<!backendApi.ReplicationControllerList>} */
+  let resource = $resource('api/v1/replicationcontrollers');
+  return resource.get().$promise;
+}
---
+++ src/test/frontend/replicationcontrollerlist/replicationcontrollercardmenu_component_test.js
@@ -0,0 +1,103 @@
+// Copyright 2015 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import {StateParams} from 'replicationcontrollerdetail/replicationcontrollerdetail_state';
+import replicationControllerListModule from 'replicationcontrollerlist/replicationcontrollerlist_module';
+
+describe('Replication controller card menu controller', () => {
+  /** @type
+   * {!replicationcontrollerlist/replicationcontrollercardmenu_component.ReplicationControllerCardMenuController}
+   */
+  let ctrl;
+  /** @type {!ui.router.$state} */
+  let state;
+  /** @type {!angular.Scope} */
+  let scope;
+  /** @type
+   * {!replicationcontrollerdetail/replicationcontroller_service.ReplicationControllerService} */
+  let kdReplicationControllerService;
+
+  beforeEach(() => {
+    angular.mock.module(replicationControllerListModule.name);
+
+    angular.mock.inject(
+        ($componentController, $state, _kdReplicationControllerService_, $rootScope) => {
+          state = $state;
+          kdReplicationControllerService = _kdReplicationControllerService_;
+          scope = $rootScope;
+          ctrl = $componentController(
+              'kdReplicationControllerCardMenu', null,
+              {replicationController: {name: 'foo-name', namespace: 'foo-namespace'}});
+        });
+  });
+
+  it('should view details', () => {
+    // given
+    spyOn(state, 'go');
+
+    // when
+    ctrl.viewDetails();
+
+    // then
+    expect(state.go).toHaveBeenCalledWith(
+        'replicationcontrollerdetail', new StateParams('foo-namespace', 'foo-name'));
+  });
+
+  it('should open the menu', () => {
+    // given
+    let openMenuFn = jasmine.createSpy();
+    let event = {};
+
+    // when
+    ctrl.openMenu(openMenuFn, event);
+
+    // then
+    expect(openMenuFn).toHaveBeenCalledWith(event);
+  });
+
+  it('should reload on successful delete', angular.mock.inject(($q) => {
+    // given
+    let deferred = $q.defer();
+    spyOn(state, 'reload');
+    spyOn(kdReplicationControllerService, 'showDeleteDialog').and.returnValue(deferred.promise);
+
+    // when
+    ctrl.showDeleteDialog();
+    deferred.resolve();
+
+    // then
+    expect(state.reload).not.toHaveBeenCalled();
+    scope.$apply();
+    expect(state.reload).toHaveBeenCalled();
+  }));
+
+  it('should show update replicas dialog', () => {
+    // given
+    ctrl.replicationController = {
+      namespace: '',
+      name: '',
+      pods: {
+        current: 1,
+        desired: 1,
+      },
+    };
+    spyOn(kdReplicationControllerService, 'showUpdateReplicasDialog');
+
+    // when
+    ctrl.showUpdateReplicasDialog();
+
+    // then
+    expect(kdReplicationControllerService.showUpdateReplicasDialog).toHaveBeenCalled();
+  });
+});
