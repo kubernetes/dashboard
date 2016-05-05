3aed4002983be21f3bd19e5d61c747bc7875bfdf
Sebastian Florek <sebastian.florek@ts.fujitsu.com>
2016-05-05 15:54:31 +0200

    Add pods card list component

---
+++ src/app/frontend/podslist/podscardlist_component.js
@@ -0,0 +1,58 @@
+// Copyright 2015 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/**
+ * @final
+ */
+export class PodsCardListController {
+  /**
+   * @ngInject
+   */
+  constructor() {
+    /**
+     * List of pods. Initialized from the scope.
+     * @export {!Array<!backendApi.Pod>}
+     */
+    this.pods;
+
+    /**
+     * Callback function that returns link to pod logs. Initialized from the scope.
+     * @export {!function({pod: !backendApi.Pod}): string}
+     */
+    this.logsHrefFn;
+  }
+
+  /**
+   * @param {!backendApi.Pod} pod
+   * @return {string}
+   * @export
+   */
+  getPodLogsHref(pod) { return this.logsHrefFn({pod: pod}); }
+}
+
+/**
+ * Definition object for the component that displays pods list card.
+ *
+ * @type {!angular.Component}
+ */
+export const podsCardListComponent = {
+  templateUrl: 'podslist/podscardlist.html',
+  controller: PodsCardListController,
+  bindings: {
+    /** {!Array<!backendApi.Pod>} */
+    'pods': '<',
+    /** {!function({pod: !backendApi.Pod}): string} */
+    'logsHrefFn': '&',
+  },
+};
