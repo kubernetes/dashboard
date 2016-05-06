180a062dedaef840f234580cd8ba5ed799b9379d
bryk <bryk@google.com>
2016-05-06 11:09:49 +0200

    Frontend for deployment list and workloads
    This looks much like a compy&paste (which is mostly true), but that's
    the plan. I want to implement the list pages now and then extract common
    patterns, if any.

---
+++ src/app/frontend/deploymentlist/deploymentlist_stateconfig.js
@@ -0,0 +1,62 @@
+// Copyright 2015 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import {actionbarViewName} from 'chrome/chrome_state';
+import {breadcrumbsConfig} from 'common/components/breadcrumbs/breadcrumbs_component';
+import {DeploymentListController} from './deploymentlist_controller';
+import {stateName, stateUrl} from './deploymentlist_state';
+import {DeploymentListActionBarController} from './deploymentlistactionbar_controller';
+
+/**
+ * Configures states for the service view.
+ *
+ * @param {!ui.router.$stateProvider} $stateProvider
+ * @ngInject
+ */
+export default function stateConfig($stateProvider) {
+  $stateProvider.state(stateName, {
+    url: stateUrl,
+    resolve: {
+      'deployments': resolveDeployments,
+    },
+    data: {
+      [breadcrumbsConfig]: {
+        'label': 'Deployments',
+      },
+    },
+    views: {
+      '': {
+        controller: DeploymentListController,
+        controllerAs: '$ctrl',
+        templateUrl: 'deploymentlist/deploymentlist.html',
+      },
+      [actionbarViewName]: {
+        controller: DeploymentListActionBarController,
+        controllerAs: 'ctrl',
+        templateUrl: 'deploymentlist/deploymentlistactionbar.html',
+      },
+    },
+  });
+}
+
+/**
+ * @param {!angular.$resource} $resource
+ * @return {!angular.$q.Promise}
+ * @ngInject
+ */
+export function resolveDeployments($resource) {
+  /** @type {!angular.Resource<!backendApi.DeploymentList>} */
+  let resource = $resource('api/v1/deployments');
+  return resource.get().$promise;
+}
---
+++ src/test/frontend/deploymentlist/deploymentlist_controller_test.js
@@ -0,0 +1,29 @@
+// Copyright 2015 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import {DeploymentListController} from 'deploymentlist/deploymentlist_controller';
+import deploymentListModule from 'deploymentlist/deploymentlist_module';
+
+describe('Replica Set list controller', () => {
+
+  beforeEach(() => { angular.mock.module(deploymentListModule.name); });
+
+  it('should initialize replication controllers', angular.mock.inject(($controller) => {
+    let ctrls = {};
+    /** @type {!DeploymentListController} */
+    let ctrl = $controller(DeploymentListController, {deployments: {deployments: ctrls}});
+
+    expect(ctrl.deployments).toBe(ctrls);
+  }));
+});
