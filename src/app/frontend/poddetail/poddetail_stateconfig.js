d8d1ee45dd475dcdb2a944e25a9e60ed97f306f0
digitalfishpond <denis.poisson@ts.fujitsu.com>
2016-05-13 10:55:21 +0200

    Add pod details page

---
+++ src/app/frontend/poddetail/poddetail_stateconfig.js
@@ -0,0 +1,66 @@
+// Copyright 2015 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import {actionbarViewName} from 'chrome/chrome_state';
+import {breadcrumbsConfig} from 'common/components/breadcrumbs/breadcrumbs_component';
+import {PodDetailController} from './poddetail_controller';
+import {stateName as podList, stateUrl} from 'podlist/podlist_state';
+import {stateName} from './poddetail_state';
+
+/**
+ * Configures states for the pod details view.
+ *
+ * @param {!ui.router.$stateProvider} $stateProvider
+ * @ngInject
+ */
+export default function stateConfig($stateProvider) {
+  $stateProvider.state(stateName, {
+    url: `${stateUrl}/:namespace/:pod`,
+    resolve: {
+      'podDetailResource': getPodDetailResource,
+      'podDetail': getPodDetail,
+    },
+    data: {
+      [breadcrumbsConfig]: {
+        'label': '{{$stateParams.pod}}',
+        'parent': podList,
+      },
+    },
+    views: {
+      '': {
+        controller: PodDetailController,
+        controllerAs: 'ctrl',
+        templateUrl: 'poddetail/poddetail.html',
+      },
+      [actionbarViewName]: {},
+    },
+  });
+}
+
+/**
+ * @param {!./poddetail_state.StateParams} $stateParams
+ * @param {!angular.$resource} $resource
+ * @return {!angular.Resource<!backendApi.PodDetail>}
+ */
+export function getPodDetailResource($resource, $stateParams) {
+  return $resource(`api/v1/pods/${$stateParams.namespace}/${$stateParams.pod}`);
+}
+
+/**
+ * @param {!angular.Resource<!backendApi.PodDetail>} podDetailResource
+ * @return {!angular.$q.Promise}
+ */
+export function getPodDetail(podDetailResource) {
+  return podDetailResource.get().$promise;
+}
--- src/app/frontend/podlist/podcardlist_component.js
+++ src/app/frontend/podlist/podcardlist_component.js
@@ -12,6 +12,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+import {StateParams} from 'poddetail/poddetail_state';
+import {stateName} from 'poddetail/poddetail_state';
+
 /**
  * @final
  */
@@ -19,7 +22,7 @@ export class PodCardListController {
   /**
    * @ngInject
    */
-  constructor() {
+  constructor($state) {
     /**
      * List of pods. Initialized from the scope.
      * @export {!backendApi.PodList}
@@ -31,6 +34,9 @@ export class PodCardListController {
      * @export {!function({pod: !backendApi.Pod}): string}
      */
     this.logsHrefFn;
+
+    /** @private {!ui.router.$state} */
+    this.state_ = $state;
   }
 
   /**
@@ -39,6 +45,16 @@ export class PodCardListController {
    * @export
    */
   getPodLogsHref(pod) { return this.logsHrefFn({pod: pod}); }
+
+  /**
+   * @param {!backendApi.Pod} pod
+   * @return {string}
+   * @export
+   */
+  getPodDetailHref(pod) {
+    return this.state_.href(
+        stateName, new StateParams(pod.objectMeta.namespace, pod.objectMeta.name));
+  }
 }
 
 /**
