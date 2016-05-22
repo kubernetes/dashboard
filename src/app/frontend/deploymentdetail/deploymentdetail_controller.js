ff74a0196467c27d4f8f60d952a074d7849c51b5
Luiz Felipe G. Pereira <luiz.felipe.gp@gmail.com>
2016-05-22 19:08:16 -0300

    Initial deployment detail module

---
+++ src/app/frontend/deploymentdetail/deploymentdetail_controller.js
@@ -0,0 +1,13 @@
+
+/**
+ * @final
+ */
+export class DeploymentDetailController {
+  /**
+   * @param {!backendApi.DeploymentDetail} deploymentDetail
+   */
+  constructor(deploymentDetail) {
+    /** @export {!backendApi.DeploymentDetail} */
+    this.deploymentDetail = deploymentDetail;
+  }
+}
---
+++ src/app/frontend/deploymentdetail/deploymentinfo_component.js
@@ -0,0 +1,37 @@
+/**
+ * @final
+ */
+export default class DeploymentInfoController {
+  /**
+   * Constructs replication controller info object.
+   */
+  constructor() {
+    /**
+     * Deployment details. Initialized from the scope.
+     * @export {!backendApi.DeploymentDetail}
+     */
+    this.deployment;
+  }
+
+  /**
+   * @return {boolean}
+   * @export
+   */
+  rollingUpdateStrategy() {
+    return this.deployment.strategy === "RollingUpdate";
+  }
+}
+
+/**
+ * Definition object for the component that displays replica set info.
+ *
+ * @return {!angular.Directive}
+ */
+export const deploymentInfoComponent = {
+  controller: DeploymentInfoController,
+  templateUrl: 'deploymentdetail/deploymentinfo.html',
+  bindings: {
+    /** {!backendApi.DeploymentDetail} */
+    'deployment': '=',
+  },
+};
