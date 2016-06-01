d3204200b7ee79f4966055562ca6bc4dff3dbc29
Marcin Maciaszczyk <Marcin.Maciaszczyk@ts.fujitsu.com>
2016-06-01 13:47:51 +0200

    Add Job resource backend

---
+++ src/app/backend/resource/job/joblist.go
@@ -0,0 +1,131 @@
+// Copyright 2015 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package job
+
+import (
+	"log"
+
+	"github.com/kubernetes/dashboard/resource/common"
+	"k8s.io/kubernetes/pkg/api"
+	k8serrors "k8s.io/kubernetes/pkg/api/errors"
+	"k8s.io/kubernetes/pkg/apis/batch"
+	client "k8s.io/kubernetes/pkg/client/unversioned"
+)
+
+// JobList contains a list of Jobs in the cluster.
+type JobList struct {
+	// Unordered list of Jobs.
+	Jobs []Job `json:"jobs"`
+}
+
+// Job is a presentation layer view of Kubernetes Job resource. This means
+// it is Job plus additional augumented data we can get from other sources
+// (like services that target the same pods).
+type Job struct {
+	ObjectMeta common.ObjectMeta `json:"objectMeta"`
+	TypeMeta   common.TypeMeta   `json:"typeMeta"`
+
+	// Aggregate information about pods belonging to this Job.
+	Pods common.PodInfo `json:"pods"`
+
+	// Container images of the Job.
+	ContainerImages []string `json:"containerImages"`
+}
+
+// GetJobList returns a list of all Jobs in the cluster.
+func GetJobList(client client.Interface, nsQuery *common.NamespaceQuery) (*JobList, error) {
+	log.Printf("Getting list of all jobs in the cluster")
+
+	channels := &common.ResourceChannels{
+		JobList:     common.GetJobListChannel(client.Extensions(), nsQuery, 1),
+		ServiceList: common.GetServiceListChannel(client, nsQuery, 1),
+		PodList:     common.GetPodListChannel(client, nsQuery, 1),
+		EventList:   common.GetEventListChannel(client, nsQuery, 1),
+		NodeList:    common.GetNodeListChannel(client, nsQuery, 1),
+	}
+
+	return GetJobListFromChannels(channels)
+}
+
+// GetJobList returns a list of all Jobs in the cluster
+// reading required resource list once from the channels.
+func GetJobListFromChannels(channels *common.ResourceChannels) (
+	*JobList, error) {
+
+	jobs := <-channels.JobList.List
+	if err := <-channels.JobList.Error; err != nil {
+		statusErr, ok := err.(*k8serrors.StatusError)
+		if ok && statusErr.ErrStatus.Reason == "NotFound" {
+			// NotFound - this means that the server does not support Job objects, which
+			// is fine.
+			emptyList := &JobList{
+				Jobs: make([]Job, 0),
+			}
+			return emptyList, nil
+		}
+		return nil, err
+	}
+
+	services := <-channels.ServiceList.List
+	if err := <-channels.ServiceList.Error; err != nil {
+		return nil, err
+	}
+
+	pods := <-channels.PodList.List
+	if err := <-channels.PodList.Error; err != nil {
+		return nil, err
+	}
+
+	events := <-channels.EventList.List
+	if err := <-channels.EventList.Error; err != nil {
+		return nil, err
+	}
+
+	nodes := <-channels.NodeList.List
+	if err := <-channels.NodeList.Error; err != nil {
+		return nil, err
+	}
+
+	return ToJobList(jobs.Items, services.Items, pods.Items, events.Items,
+		nodes.Items), nil
+}
+
+func ToJobList(jobs []batch.Job,
+	services []api.Service, pods []api.Pod, events []api.Event,
+	nodes []api.Node) *JobList {
+
+	jobList := &JobList{
+		Jobs: make([]Job, 0),
+	}
+
+	for _, job := range jobs {
+		matchingPods := common.FilterNamespacedPodsBySelector(pods, job.ObjectMeta.Namespace,
+			job.Spec.Selector.MatchLabels)
+		podInfo := getPodInfo(&job, matchingPods)
+
+		jobList.Jobs = append(jobList.Jobs, ToJob(&job, &podInfo))
+	}
+
+	return jobList
+}
+
+func ToJob(job *batch.Job, podInfo *common.PodInfo) Job {
+	return Job{
+		ObjectMeta:      common.NewObjectMeta(job.ObjectMeta),
+		TypeMeta:        common.NewTypeMeta(common.ResourceKindJob),
+		ContainerImages: common.GetContainerImages(&job.Spec.Template.Spec),
+		Pods:            *podInfo,
+	}
+}
