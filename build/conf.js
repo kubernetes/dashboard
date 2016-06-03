603e542d37c103c6f9a2e5c499a4d6e3d9a99602
Piotr Bryk <bryk@google.com>
2016-06-03 15:23:01 +0200

    Change version to beta2 in preparation for release (#810)

--- build/conf.js
+++ build/conf.js
@@ -49,7 +49,7 @@ const version = {
   /**
    * Current release version of the project.
    */
-  release: 'v1.1.0-beta1',
+  release: 'v1.1.0-beta2',
   /**
    * Version name of the canary release of the project.
    */
