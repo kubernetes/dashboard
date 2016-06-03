b73a6ce6037d06119f92b97df8f2f125587d8cfa
Sebastian Florek <sebastian.florek@ts.fujitsu.com>
2016-06-03 08:50:50 +0200

    Add pet set detail page

---
+++ src/app/frontend/petsetdetail/petsetdetail_stateconfig.js
@@ -0,0 +1,70 @@
+// Copyright 2015 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import {actionbarViewName, stateName as chromeStateName} from 'chrome/chrome_state';
+import {breadcrumbsConfig} from 'common/components/breadcrumbs/breadcrumbs_service';
+import {stateName as petSetList, stateUrl} from 'petsetlist/petsetlist_state';
+
+import {PetSetDetailController} from './petsetdetail_controller';
+import {stateName} from './petsetdetail_state';
+
+/**
+ * Configures states for the pet set details view.
+ *
+ * @param {!ui.router.$stateProvider} $stateProvider
+ * @ngInject
+ */
+export default function stateConfig($stateProvider) {
+  $stateProvider.state(stateName, {
+    url: `${stateUrl}/:namespace/:petSet`,
+    parent: chromeStateName,
+    resolve: {
+      'petSetDetailResource': getPetSetDetailResource,
+      'petSetDetail': getPetSetDetail,
+    },
+    data: {
+      [breadcrumbsConfig]: {
+        'label': '{{$stateParams.petSet}}',
+        'parent': petSetList,
+      },
+    },
+    views: {
+      '': {
+        controller: PetSetDetailController,
+        controllerAs: 'ctrl',
+        templateUrl: 'petsetdetail/petsetdetail.html',
+      },
+      [actionbarViewName]: {},
+    },
+  });
+}
+
+/**
+ * @param {!./petsetdetail_state.StateParams} $stateParams
+ * @param {!angular.$resource} $resource
+ * @return {!angular.Resource<!backendApi.PetSetDetail>}
+ * @ngInject
+ */
+export function getPetSetDetailResource($resource, $stateParams) {
+  return $resource(`api/v1/petset/${$stateParams.namespace}/${$stateParams.petSet}`);
+}
+
+/**
+ * @param {!angular.Resource<!backendApi.PetSetDetail>} petSetDetailResource
+ * @return {!angular.$q.Promise}
+ * @ngInject
+ */
+export function getPetSetDetail(petSetDetailResource) {
+  return petSetDetailResource.get().$promise;
+}
---
+++ src/app/frontend/petsetdetail/petsetinfo_component.js
@@ -0,0 +1,49 @@
+// Copyright 2015 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/**
+ * @final
+ */
+export default class PetSetInfoController {
+  /**
+   * Constructs pettion controller info object.
+   */
+  constructor() {
+    /**
+     * Pet set details. Initialized from the scope.
+     * @export {!backendApi.PetSetDetail}
+     */
+    this.petSet;
+  }
+
+  /**
+   * @return {boolean}
+   * @export
+   */
+  areDesiredPodsRunning() { return this.petSet.podInfo.running === this.petSet.podInfo.desired; }
+}
+
+/**
+ * Definition object for the component that displays pet set info.
+ *
+ * @return {!angular.Directive}
+ */
+export const petSetInfoComponent = {
+  controller: PetSetInfoController,
+  templateUrl: 'petsetdetail/petsetinfo.html',
+  bindings: {
+    /** {!backendApi.PetSetDetail} */
+    'petSet': '=',
+  },
+};
