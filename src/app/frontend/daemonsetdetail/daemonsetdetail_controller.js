eca4e8065193455386710a5d0c34adb326c70470
Atanas Mirchev <atanas.mirchev@est.fujitsu.com>
2016-06-06 14:54:19 +0200

    Localization of DaemonSetList and -Details views

--- src/app/frontend/daemonsetdetail/daemonsetdetail_controller.js
+++ src/app/frontend/daemonsetdetail/daemonsetdetail_controller.js
@@ -23,5 +23,20 @@ export class DaemonSetDetailController {
   constructor(daemonSetDetail) {
     /** @export {!backendApi.DaemonSet} */
     this.daemonSetDetail = daemonSetDetail;
+
+    /** @export */
+    this.i18n = i18n;
   }
 }
+
+const i18n = {
+  /** @export {string} @desc Label 'Overview' on the left navigation tab on the daemon set detail
+     page. */
+  MSG_DAEMON_SET_DETAIL_OVERVIEW_LABEL: goog.getMsg('Overview'),
+  /** @export {string} @desc Title 'Pods' for the pods information section on the daemon set detail
+     page. */
+  MSG_DAEMON_SET_DETAIL_PODS_TITLE: goog.getMsg('Pods'),
+  /** @export {string} @desc Label 'Events' on the right navigation tab on the daemon set detail
+     page. */
+  MSG_DAEMON_SET_DETAIL_EVENTS_LABEL: goog.getMsg('Events'),
+};
