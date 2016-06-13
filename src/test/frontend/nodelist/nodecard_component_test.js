9570da901183954a13185092ff591962f03025f9
Marcin Maciaszczyk <Marcin.Maciaszczyk@ts.fujitsu.com>
2016-06-13 14:28:06 +0200

    Add node resource frontend

---
+++ src/test/frontend/nodelist/nodecard_component_test.js
@@ -0,0 +1,46 @@
+// Copyright 2015 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+import nodeListModule from 'nodelist/nodelist_module';
+
+describe('Node card', () => {
+  /**
+   * @type
+   * {!nodelist/nodecard_component.NodeCardController}
+   */
+  let ctrl;
+
+  beforeEach(() => {
+    angular.mock.module(nodeListModule.name);
+
+    angular.mock.inject(($componentController, $rootScope) => {
+      ctrl = $componentController('kdNodeCard', {$scope: $rootScope});
+    });
+  });
+
+  it('should construct details href', () => {
+    // given
+    ctrl.node = {
+      objectMeta: {
+        name: 'foo-name',
+      },
+    };
+
+    // then
+    expect(ctrl.getNodeDetailHref()).toEqual('#/node/foo-name');
+  });
+
+  it('should format the "created at" tooltip correctly'), () => {
+    expect(ctrl.getCreatedAtTooltip('2016-06-06T09:13:12Z')).toEqual('Created at 6/6/16 09:13 AM');
+  };
+});
