e4a66f9bdde4e30600d682e14a5642b011b7d22e
bryk <bryk@google.com>
2016-06-22 08:31:15 +0200

    A couple of fixes in zerostate code
    1. Make the link include namespace that is selected. We should use
    `$state` for all links, anyway.
    1. Increase padding to make the information more prominent.
    1. Add info that in order to see something you can change namespace.
    1. Align external link icon vertically
    1. Change wording to be similar to what we have in other places (e.g.,
    "deploy a containerized app".

--- src/app/frontend/common/components/zerostate/zerostate_component.js
+++ src/app/frontend/common/components/zerostate/zerostate_component.js
@@ -12,23 +12,24 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+import {deployAppStateName} from 'deploy/deploy_state';
+
 /**
  * @final
  */
 export class ZeroStateController {
   /**
+   * @param {!ui.router.$state} $state
    * @ngInject
    */
-  constructor() {
+  constructor($state) {
     /** @export */
-    this.i18n = i18n;
+    this.i18n = i18n($state);
   }
 }
 
 /**
- * Returns zero state component.
- *
- * @return {!angular.Directive}
+ * @return {!angular.Component}
  */
 export const zeroStateComponent = {
   templateUrl: 'common/components/zerostate/zerostate.html',
@@ -36,19 +37,23 @@ export const zeroStateComponent = {
   controller: ZeroStateController,
 };
 
-const i18n = {
+/**
+ * @param {!ui.router.$state} $state
+ */
+let i18n = ($state) => ({
   /** @export {string} @desc Title text which appears on zero state view. */
   MSG_ZERO_STATE_TITLE: goog.getMsg('There is nothing to display here'),
   /** @export {string} @desc Text which appears on zero state view under the title. It provides
    *  to deploy view or offers user to take the Dashboard Tour. */
   MSG_ZERO_STATE_TEXT: goog.getMsg(
-      'Let\'s {$createNewLink} create a new app {$createNewCloseLink} or learn more' +
-          ' and {$openLink} take the Dashboard Tour {$linkIcon} {$closeLink}',
+      'You can {$createNewLink}deploy a containerized app{$createNewCloseLink},' +
+          ' select other namespace or {$openLink}take the Dashboard Tour {$linkIcon}{$closeLink}' +
+          ' to learn more',
       {
-        'createNewLink': `<a href="#/deploy/file">`,
+        'createNewLink': `<a href="${$state.href(deployAppStateName)}">`,
         'createNewCloseLink': `</a>`,
         'openLink': `<a href="http://kubernetes.io/docs/user-guide/ui/" target="_blank">`,
         'linkIcon': `<i class="material-icons kd-zerostate-icon">open_in_new</i>`,
         'closeLink': `</a>`,
       }),
-};
+});
