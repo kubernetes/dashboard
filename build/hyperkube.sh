0fed74ec7c044e27bad4f6c7879e80852f101c88
Christoph Held <christoph.held@est.fujitsu.com>
2016-06-28 10:17:35 +0200

    try docker 1.11

--- build/hyperkube.sh
+++ build/hyperkube.sh
@@ -30,7 +30,7 @@ docker run \
     --volume=/sys:/sys:ro \
     --volume=/dev:/dev \
     --volume=/var/lib/docker/:/var/lib/docker:ro \
-    --volume=/var/lib/kubelet/:/var/lib/kubelet:rw,rslave \
+    --volume=/var/lib/kubelet/:/var/lib/kubelet:rw \
     --volume=/var/run:/var/run:rw \
     --net=host \
     --pid=host \
