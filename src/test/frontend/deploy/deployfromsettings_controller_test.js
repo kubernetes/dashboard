011f17a2b1abbee866f2c4746cfd9c5a4d259e50
PiotrDabkowski <pdabkowski@google.com>
2016-07-08 10:24:19 +0200

    Updated test cases for deployfromsettings controller

--- src/test/frontend/deploy/deployfromsettings_controller_test.js
+++ src/test/frontend/deploy/deployfromsettings_controller_test.js
@@ -334,15 +334,15 @@ describe('DeployFromSettings controller', () => {
 
     it('get secrets should update the secrets list', () => {
       ctrl.resource_ = angularResource;
-      let response = {
-        secrets: ['secret1', 'secret2', 'secret3'],
-      };
+      let response = { "secrets": [{"metadata": {"name": "secret1"}},
+          {"metadata": {"name": "secret2"}},
+          {"metadata": {"name": "secret3"}}]};
       httpBackend.expectGET('api/v1/secret/default').respond(200, response);
       // when
       ctrl.getSecrets('default');
       httpBackend.flush();
       // expect
-      expect(ctrl.secrets).toEqual(response.secrets);
+      expect(ctrl.secrets).toEqual(["secret1", "secret2", "secret3"]);
     });
 
     it('successful image pull secret creation should update ctrl.imagePullSecret', () => {
