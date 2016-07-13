0d7811b27d7aa09ba112ea71a795d74731dd323a
PiotrDabkowski <pdabkowski@google.com>
2016-07-13 11:11:55 +0200

    Added secrets frontend.
    List of secrets can now be found under #/secrets?namespace=namespace.

---
+++ src/app/frontend/secretlist/secretcardlist_component.js
@@ -0,0 +1,55 @@
+// Copyright 2015 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/**
+ * @final
+ */
+export class SecretCardListController {
+  /**
+   * @ngInject
+   */
+  constructor() {
+    /**
+     * List of secrets. Initialized from the scope.
+     * @export {!backendApi.SecretList}
+     */
+    this.secretList;
+
+    /** @export */
+    this.i18n = i18n;
+  }
+}
+
+/**
+ * Definition object for the component that displays pods list card.
+ *
+ * @type {!angular.Component}
+ */
+export const secretCardListComponent = {
+  templateUrl: 'secretlist/secretcardlist.html',
+  controller: SecretCardListController,
+  bindings: {
+    /** {!backendApi.SecretList} */
+    'secretList': '<',
+  },
+};
+
+const i18n = {
+  /** @export {string} @desc Label 'Name' which appears as a column label in the table of
+   pods (pod list view). */
+  MSG_SECRET_LIST_NAME_LABEL: goog.getMsg('Name'),
+  /** @export {string} @desc Label 'Age' which appears as a column label in the
+   table of pods (pod list view). */
+  MSG_SECRET_LIST_AGE_LABEL: goog.getMsg('Age'),
+};
