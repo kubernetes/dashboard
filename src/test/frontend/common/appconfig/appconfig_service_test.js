09174e9939438bbad3e28a9622b5b6d7f13a85c1
bryk <bryk@google.com>
2016-07-19 09:44:47 +0200

    Fix relative dates and align them with commandline
    * change "didn't happen yet" to "-"
    * change "just now" to "0 seconds" (this is how CLI works)
    * use "0 seconds" for now() -+ 1second
    * increment time over the live of application
    
    Fixes #1043

---
+++ src/test/frontend/common/appconfig/appconfig_service_test.js
@@ -0,0 +1,45 @@
+// Copyright 2015 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import module from 'common/appconfig/appconfig_module';
+import {AppConfigService} from 'common/appconfig/appconfig_service';
+
+describe('Appconfig service test', () => {
+  it(`increment time when getting server time`, () => {
+    angular.mock.module(module.name);
+
+    /**
+     * Current time mock.
+     * @type {Date}
+     */
+    const currentTime = new Date(
+        2015,  // year
+        11,    // month
+        29,    // day
+        20,    // hour
+        30,    // minute
+        40     // second
+        );
+    jasmine.clock().mockDate(currentTime);
+    /** @type {!common/appconfig/appconfig_service.AppConfigService} */
+    let kdAppConfigService = new AppConfigService({serverTime: 1234567890123});
+
+    expect(kdAppConfigService.getServerTime().getTime()).toBe(1234567890123);
+
+    currentTime.setSeconds(20);
+    jasmine.clock().mockDate(currentTime);
+
+    expect(kdAppConfigService.getServerTime().getTime()).toBe(1234567890123 - 20 * 1000);
+  });
+});
