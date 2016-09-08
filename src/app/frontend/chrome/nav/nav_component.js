6865f5fa8cac64c624c0c8bd2b018de2a9aca42c
Piotr Bryk <bryk@google.com>
2016-09-08 15:05:13 +0200

    Fix redirect-loop issue that happens on errors (#1203) (#1204)
    * Fix redirect-loop issue that happens on errors (#1203)
    
    Previously to reprodce:
    1. Start a proxy and a dev server
    2. Kill the proxy
    3. Open the UI
    4. See that it is in a redirect loop

--- src/app/frontend/chrome/nav/nav_component.js
+++ src/app/frontend/chrome/nav/nav_component.js
@@ -61,6 +61,9 @@ export class NavController {
       'configMap': configMapState,
       'ingress': ingressState,
     };
+
+    /** @export */
+    this.i18n = i18n;
   }
 
   /** @export */
@@ -78,4 +81,47 @@ export class NavController {
 export const navComponent = {
   controller: NavController,
   templateUrl: 'chrome/nav/nav.html',
+};
+
+const i18n = {
+  /** @export @desc Admin menu item in the nav. */
+  MSG_NAV_MENU_ADMIN: goog.getMsg('Admin'),
+  /** @export @desc Namespaces menu item in the nav. */
+  MSG_NAV_MENU_NAMESPACES: goog.getMsg('Namespaces'),
+  /** @export @desc Nodes menu item in the nav. */
+  MSG_NAV_MENU_NODES: goog.getMsg('Nodes'),
+  /** @export @desc Persistent Volumes menu item in the nav. */
+  MSG_NAV_MENU_PERSISTENT_VOLUMES: goog.getMsg('Persistent Volumes'),
+  /** @export @desc Workloads menu item in the nav. */
+  MSG_NAV_MENU_WORKLOADS: goog.getMsg('Workloads'),
+  /** @export @desc Deployments menu item in the nav. */
+  MSG_NAV_MENU_DEPLOYMENTS: goog.getMsg('Deployments'),
+  /** @export @desc Replica Sets menu item in the nav. */
+  MSG_NAV_MENU_REPLICA_SETS: goog.getMsg('Replica Sets'),
+  /** @export @desc Replication Controllers menu item in the nav. */
+  MSG_NAV_MENU_REPLICATION_CONTROLLERS: goog.getMsg('Replication Controllers'),
+  /** @export @desc Daemon Sets menu item in the nav. */
+  MSG_NAV_MENU_DAEMON_SETS: goog.getMsg('Daemon Sets'),
+  /** @export @desc Pet Sets menu item in the nav. */
+  MSG_NAV_MENU_PET_SETS: goog.getMsg('Pet Sets'),
+  /** @export @desc Jobs menu item in the nav. */
+  MSG_NAV_MENU_JOBS: goog.getMsg('Jobs'),
+  /** @export @desc Pods menu item in the nav. */
+  MSG_NAV_MENU_PODS: goog.getMsg('Pods'),
+  /** @export @desc Services and discovery menu item in the nav. */
+  MSG_NAV_MENU_SERVICES_DISCOVERY: goog.getMsg('Services and discovery'),
+  /** @export @desc Services menu item in the nav. */
+  MSG_NAV_MENU_SERVICES: goog.getMsg('Services'),
+  /** @export @desc Ingress item in the nav. */
+  MSG_NAV_MENU_INGRESS: goog.getMsg('Ingress'),
+  /** @export @desc Storage item in the nav. */
+  MSG_NAV_MENU_STORAGE: goog.getMsg('Storage'),
+  /** @export @desc Persistent Volume Claims item in the nav. */
+  MSG_NAV_MENU_PVCS: goog.getMsg('Persistent Volume Claims'),
+  /** @export @desc Config item in the nav. */
+  MSG_NAV_MENU_CONFIG: goog.getMsg('Config'),
+  /** @export @desc Secrets item in the nav. */
+  MSG_NAV_MENU_SECRETS: goog.getMsg('Secrets'),
+  /** @export @desc Config Maps item in the nav. */
+  MSG_NAV_MENU_CONFIG_MAPS: goog.getMsg('Config Maps'),
 };
