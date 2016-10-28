8c83a03a1b84fd18830b8bfaf1ea09ba5cc93e78
Piotr Bryk <bryk@google.com>
2016-10-28 13:04:06 +0200

    Build and push HEAD image on every build on master  (#1377)
    * Adjust travis to push canary image on successful build
    
    * Build and push HEAD image on every build on master
    
    Also rename canary to head for images that we build. This name conveys
    better what we actually do.
    
    * Remove Google SDK reference from the travis config

---
+++ build/ci.js
@@ -0,0 +1,43 @@
+// Copyright 2015 Google Inc. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/**
+ * @fileoverview Entrypoint tasks for ci environments.
+ */
+import childProcess from 'child_process';
+import gulp from 'gulp';
+
+/**
+ * Entry point task for CI environments.
+ */
+gulp.task('ci', ['check:local-cluster'], (doneFn) => {
+  if (process.env.TRAVIS) {
+    // Pushe head images when docker user and password are available.
+    if (process.env.TRAVIS_PULL_REQUEST === 'false' && process.env.DOCKER_USER &&
+        process.env.DOCKER_PASS) {
+      childProcess.exec('docker login -u $DOCKER_USER -p $DOCKER_PASS', (err, stdout, stderr) => {
+        if (err) {
+          doneFn(new Error(`Cannot login to docker: ${err}, ${stdout}, ${stderr}`));
+        } else {
+          gulp.start('push-to-docker:head:cross');
+          doneFn();
+        }
+      });
+    } else {
+      doneFn();
+    }
+  } else {
+    doneFn(new Error('Not in a CI environment (such as Travis). Aborting.'));
+  }
+});
