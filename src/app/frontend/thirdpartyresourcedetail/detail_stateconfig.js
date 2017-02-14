6b5c4f926dda5ae0e9985c063f992736e0800814
Marcin Maciaszczyk <marcin.maciaszczyk@ts.fujitsu.com>
2017-02-14 13:30:30 +0100

    Implement third party resource detail page

---
+++ src/app/frontend/thirdpartyresourcedetail/detail_stateconfig.js
@@ -0,0 +1,71 @@
+// Copyright 2015 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import {stateName as chromeStateName} from 'chrome/chrome_state';
+import {breadcrumbsConfig} from 'common/components/breadcrumbs/breadcrumbs_service';
+import {appendDetailParamsToUrl} from 'common/resource/resourcedetail';
+import {stateName as tprListState, stateUrl} from 'thirdpartyresourcelist/list_state';
+
+import {ThirdPartyResourceDetailController} from './detail_controller';
+import {stateName} from './detail_state';
+
+/**
+ * Configures states for the Third Party Resource detail view.
+ *
+ * @param {!ui.router.$stateProvider} $stateProvider
+ * @ngInject
+ */
+export default function stateConfig($stateProvider) {
+  $stateProvider.state(stateName, {
+    url: appendDetailParamsToUrl(stateUrl),
+    parent: chromeStateName,
+    resolve: {
+      'tprDetailResource': getTprDetailResource,
+      'tprDetail': getTprDetail,
+    },
+    data: {
+      [breadcrumbsConfig]: {
+        'label': '{{$stateParams.objectName}}',
+        'parent': tprListState,
+      },
+    },
+    views: {
+      '': {
+        controller: ThirdPartyResourceDetailController,
+        controllerAs: '$ctrl',
+        templateUrl: 'thirdpartyresourcedetail/detail.html',
+      },
+    },
+  });
+}
+
+/**
+ * @param {!./../common/resource/resourcedetail.StateParams} $stateParams
+ * @param {!angular.$resource} $resource
+ * @return {!angular.Resource<!backendApi.ThirdPartyResource>}
+ * @ngInject
+ */
+export function getTprDetailResource($resource, $stateParams) {
+  // TODO(maciaszczykm): Replace with backend call.
+  return $resource('/assets/tpr-detail-mock.json');
+}
+
+/**
+ * @param {!angular.Resource<!backendApi.ThirdPartyResource>} tprDetailResource
+ * @return {!angular.$q.Promise}
+ * @ngInject
+ */
+export function getTprDetail(tprDetailResource) {
+  return tprDetailResource.get().$promise;
+}
