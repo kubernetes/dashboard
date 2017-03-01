5173933783f2a70ebacaa9f34ec54104b3dc14ec
maciaszczykm <maciaszczykm@icloud.com>
2017-03-01 14:58:30 +0100

    Update URL regex

--- src/app/frontend/common/components/labels/labels_controller.js
+++ src/app/frontend/common/components/labels/labels_controller.js
@@ -19,10 +19,18 @@
 const alwaysVisibleLabelsNumber = 5;
 
 /**
- * Regular expression matching URLs.
+ * Regular expression for URL validation created by @dperini.
+ * https://gist.github.com/dperini/729294
+ *
  * @type {RegExp}
  */
-const urlRegexp = new RegExp('(https?|ftp)://(-\.)?([^\s/?\.#-]+\.?)+(/[^\s]*)?', 'i');
+const urlRegexp = new RegExp(
+    '^(?:(?:https?|ftp)://)(?:\\S+(?::\\S*)?@)?(?:(?!(?:10|127)(?:\\.\\d{1,3}){3})(?!(?:169\\.254|192\\.168)' +
+        '(?:\\.\\d{1,3}){2})(?!172\\.(?:1[6-9]|2\\d|3[0-1])(?:\\.\\d{1,3}){2})(?:[1-9]\\d?|1\\d\\d|2[01]\\d|' +
+        '22[0-3])(?:\\.(?:1?\\d{1,2}|2[0-4]\\d|25[0-5])){2}(?:\\.(?:[1-9]\\d?|1\\d\\d|2[0-4]\\d|25[0-4]))|(?' +
+        ':(?:[a-z\\u00a1-\\uffff0-9]-*)*[a-z\\u00a1-\\uffff0-9]+)(?:\\.(?:[a-z\\u00a1-\\uffff0-9]-*)*[a-z\\u' +
+        '00a1-\\uffff0-9]+)*(?:\\.(?:[a-z\\u00a1-\\uffff]{2,}))\\.?)(?::\\d{2,5})?(?:[/?#]\\S*)?$',
+    'i');
 
 /**
  * @final
