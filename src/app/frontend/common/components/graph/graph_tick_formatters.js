660df2ac96b053d677349683520651eeed41aa47
Marcin Maciaszczyk <marcin.maciaszczyk@ts.fujitsu.com>
2017-03-30 10:04:09 +0200

    Format skipped files

--- src/app/frontend/common/components/graph/graph_tick_formatters.js
+++ src/app/frontend/common/components/graph/graph_tick_formatters.js
@@ -16,9 +16,9 @@ import coresFilter from 'common/filters/cores_filter';
 import memoryFilter from 'common/filters/memory_filter';
 
 /**
- * Formats the number to contain ideally 3 significant figures, but reduces the number of significant figures for
- * small numbers in order to keep the number of decimal places down to 3. So for numbers below 0.01 number of
- * significant figures will be 1.
+ * Formats the number to contain ideally 3 significant figures, but reduces the number of
+ * significant figures for small numbers in order to keep the number of decimal places down to 3. So
+ * for numbers below 0.01 number of significant figures will be 1.
  * @param {number} d
  * @return {string}
  * @private
