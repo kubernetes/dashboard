0814a325c4bc60a94535d1a57a2d2614d5dd6834
Marcin Maciaszczyk <marcin.maciaszczyk@ts.fujitsu.com>
2017-04-27 12:37:08 +0200

    Align with new Go deps

--- src/app/backend/resource/common/pod.go
+++ src/app/backend/resource/common/pod.go
@@ -17,7 +17,7 @@ package common
 import (
 	"k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
-	apiHelpers "k8s.io/client-go/pkg/api"
+	"k8s.io/client-go/pkg/api/helper"
 	api "k8s.io/client-go/pkg/api/v1"
 	extensions "k8s.io/client-go/pkg/apis/extensions/v1beta1"
 )
@@ -111,5 +111,5 @@ func EqualIgnoreHash(template1, template2 api.PodTemplateSpec) bool {
 	}
 	// Then, compare the templates without comparing their labels
 	template1.Labels, template2.Labels = nil, nil
-	return apiHelpers.Semantic.DeepEqual(template1, template2)
+	return helper.Semantic.DeepEqual(template1, template2)
 }
--- src/app/backend/resource/deployment/util.go
+++ src/app/backend/resource/deployment/util.go
@@ -6,7 +6,6 @@ import (
 	"github.com/kubernetes/dashboard/src/app/backend/resource/common"
 	metaV1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/labels"
-	"k8s.io/client-go/pkg/api/helper"
 	"k8s.io/client-go/pkg/api/v1"
 	extensions "k8s.io/client-go/pkg/apis/extensions/v1beta1"
 )
@@ -76,25 +75,4 @@ func GetNewReplicaSetTemplate(deployment *extensions.Deployment) v1.PodTemplateS
 		ObjectMeta: deployment.Spec.Template.ObjectMeta,
 		Spec:       deployment.Spec.Template.Spec,
 	}
-}
-
-// EqualIgnoreHash returns true if two given podTemplateSpec are equal, ignoring the diff in value of Labels[pod-template-hash]
-// We ignore pod-template-hash because the hash result would be different upon podTemplateSpec API changes
-// (e.g. the addition of a new field will cause the hash code to change)
-// Note that we assume input podTemplateSpecs contain non-empty labels
-func EqualIgnoreHash(template1, template2 v1.PodTemplateSpec) bool {
-	// First, compare template.Labels (ignoring hash)
-	labels1, labels2 := template1.Labels, template2.Labels
-	if len(labels1) > len(labels2) {
-		labels1, labels2 = labels2, labels1
-	}
-	// We make sure len(labels2) >= len(labels1)
-	for k, v := range labels2 {
-		if labels1[k] != v && k != extensions.DefaultDeploymentUniqueLabelKey {
-			return false
-		}
-	}
-	// Then, compare the templates without comparing their labels
-	template1.Labels, template2.Labels = nil, nil
-	return helper.Semantic.DeepEqual(template1, template2)
 }
