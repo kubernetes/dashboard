da571fb8ee60b47e8ca501d1cdc15ebcd2596982
Marcin Maciaszczyk <maciaszczykm@users.noreply.github.com>
2017-05-11 12:20:21 +0200

    Update dialog.js

--- src/app/frontend/common/scaling/dialog.js
+++ src/app/frontend/common/scaling/dialog.js
@@ -28,7 +28,6 @@ export default function showUpdateReplicasDialog(
     mdDialog, namespace, resourceName, currentPods, desiredPods, resourceKindName) {
   return mdDialog.show({
     controller: ScaleResourceDialogController,
-    controllerAs: '$ctrl',
     clickOutsideToClose: true,
     templateUrl: 'common/scaling/scaleresource.html',
     locals: {
