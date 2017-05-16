5b0594a85e6b837a0cbba19f882d1d6146685d10
Marcin Maciaszczyk <maciaszczykm@users.noreply.github.com>
2017-05-16 14:24:38 +0200

    Increase version (#1950)

--- build/conf.js
+++ build/conf.js
@@ -59,7 +59,7 @@ const version = {
   /**
    * Current release version of the project.
    */
-  release: 'v1.6.0',
+  release: 'v1.6.1',
   /**
    * Version name of the head release of the project.
    */
