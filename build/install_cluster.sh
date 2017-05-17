d2cbc96b71727ea2b5550ed4c876eff9cc0af3d7
Christoph Held <c-held@web.de>
2017-05-17 10:07:45 +0200

    fix output2

--- build/install_cluster.sh
+++ build/install_cluster.sh
@@ -2,15 +2,20 @@
 
 echo "test"
 
-sudo apt-get update && apt-get install -y apt-transport-https
-sudo curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
-sudo cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
+sudo -s
+
+echo "test2"
+
+
+apt-get update && apt-get install -y apt-transport-https
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
 deb http://apt.kubernetes.io/ kubernetes-xenial main
 EOF
-sudo apt-get update
+apt-get update
 # Install docker if you don't have it already.
-sudo apt-get install -y kubelet kubeadm kubectl kubernetes-cni
-sudo kubeadm init
+apt-get install -y kubelet kubeadm kubectl kubernetes-cni
+kubeadm init
 
 cp /etc/kubernetes/admin.conf $HOME/
 chown $(id -u):$(id -g) $HOME/admin.conf
