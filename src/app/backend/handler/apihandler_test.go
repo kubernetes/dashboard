9a0d7e780f74e2e59b0402ab91af858fa7512e44
Marcin Maciaszczyk <marcin.maciaszczyk@ts.fujitsu.com>
2017-05-18 14:51:00 +0200

    Remove generic comments Add more tests

--- src/app/backend/handler/apihandler_test.go
+++ src/app/backend/handler/apihandler_test.go
@@ -4,7 +4,10 @@ import (
 	"net/http"
 	"testing"
 
+	"bytes"
 	"github.com/emicklei/go-restful"
+	"reflect"
+	"strings"
 )
 
 func TestCreateHTTPAPIHandler(t *testing.T) {
@@ -40,6 +43,51 @@ func TestShouldDoCsrfValidation(t *testing.T) {
 		actual := shouldDoCsrfValidation(c.request)
 		if actual != c.expected {
 			t.Errorf("shouldDoCsrfValidation(%#v) returns %#v, expected %#v", c.request, actual, c.expected)
+		}
+	}
+}
+
+func TestMapUrlToResource(t *testing.T) {
+	cases := []struct {
+		url, expected string
+	}{
+		{
+			"/api/v1/pod",
+			"pod",
+		},
+		{
+			"/api/v1/node",
+			"node",
+		},
+	}
+	for _, c := range cases {
+		actual := mapUrlToResource(c.url)
+		if !reflect.DeepEqual(actual, &c.expected) {
+			t.Errorf("mapUrlToResource(%#v) returns %#v, expected %#v", c.url, actual, c.expected)
+		}
+	}
+}
+
+func TestFormatRequestLog(t *testing.T) {
+	req, err := http.NewRequest("PUT", "/api/v1/pod", bytes.NewReader([]byte("{}")))
+	if err != nil {
+		t.Error("Cannot mockup request")
+	}
+	cases := []struct {
+		request  *restful.Request
+		expected string
+	}{
+		{
+			&restful.Request{
+				Request: req,
+			},
+			"Incoming HTTP/1.1 PUT /api/v1/pod request",
+		},
+	}
+	for _, c := range cases {
+		actual := formatRequestLog(c.request)
+		if !strings.Contains(actual, c.expected) {
+			t.Errorf("formatRequestLog(%#v) returns %#v, expected to contain %#v", c.request, actual, c.expected)
 		}
 	}
 }
