59d7d43d26fafbee04854fbbd04b814cee688db8
maciaszczykm <maciaszczykm@icloud.com>
2017-05-19 12:34:08 +0200

    Add allocated resources data to node list endpoint

--- src/app/backend/resource/node/list.go
+++ src/app/backend/resource/node/list.go
@@ -30,9 +30,7 @@ import (
 
 // NodeList contains a list of nodes in the cluster.
 type NodeList struct {
-	ListMeta common.ListMeta `json:"listMeta"`
-
-	// Unordered list of Nodes.
+	ListMeta          common.ListMeta `json:"listMeta"`
 	Nodes             []Node          `json:"nodes"`
 	CumulativeMetrics []metric.Metric `json:"cumulativeMetrics"`
 }
@@ -40,15 +38,14 @@ type NodeList struct {
 // Node is a presentation layer view of Kubernetes nodes. This means it is node plus additional
 // augmented data we can get from other sources.
 type Node struct {
-	ObjectMeta common.ObjectMeta `json:"objectMeta"`
-	TypeMeta   common.TypeMeta   `json:"typeMeta"`
-
-	// Ready Status of the node
-	Ready api.ConditionStatus `json:"ready"`
+	ObjectMeta         common.ObjectMeta      `json:"objectMeta"`
+	TypeMeta           common.TypeMeta        `json:"typeMeta"`
+	Ready              api.ConditionStatus    `json:"ready"`
+	AllocatedResources NodeAllocatedResources `json:"allocatedResources"`
 }
 
 // GetNodeListFromChannels returns a list of all Nodes in the cluster.
-func GetNodeListFromChannels(channels *common.ResourceChannels, dsQuery *dataselect.DataSelectQuery,
+func GetNodeListFromChannels(client client.Interface, channels *common.ResourceChannels, dsQuery *dataselect.DataSelectQuery,
 	heapsterClient *heapster.HeapsterClient) (*NodeList, error) {
 	log.Print("Getting node list")
 
@@ -57,7 +54,7 @@ func GetNodeListFromChannels(channels *common.ResourceChannels, dsQuery *datasel
 		return nil, err
 	}
 
-	return toNodeList(nodes.Items, dsQuery, heapsterClient), nil
+	return toNodeList(client, nodes.Items, dsQuery, heapsterClient), nil
 }
 
 // GetNodeList returns a list of all Nodes in the cluster.
@@ -73,21 +70,28 @@ func GetNodeList(client client.Interface, dsQuery *dataselect.DataSelectQuery, h
 		return nil, err
 	}
 
-	return toNodeList(nodes.Items, dsQuery, heapsterClient), nil
+	return toNodeList(client, nodes.Items, dsQuery, heapsterClient), nil
 }
 
-func toNodeList(nodes []api.Node, dsQuery *dataselect.DataSelectQuery, heapsterClient *heapster.HeapsterClient) *NodeList {
+func toNodeList(client client.Interface, nodes []api.Node, dsQuery *dataselect.DataSelectQuery,
+	heapsterClient *heapster.HeapsterClient) *NodeList {
 	nodeList := &NodeList{
 		Nodes:    make([]Node, 0),
 		ListMeta: common.ListMeta{TotalItems: len(nodes)},
 	}
 
-	nodeCells, metricPromises, filteredTotal := dataselect.GenericDataSelectWithFilterAndMetrics(toCells(nodes), dsQuery, dataselect.NoResourceCache, heapsterClient)
+	nodeCells, metricPromises, filteredTotal := dataselect.GenericDataSelectWithFilterAndMetrics(toCells(nodes),
+		dsQuery, dataselect.NoResourceCache, heapsterClient)
 	nodes = fromCells(nodeCells)
 	nodeList.ListMeta = common.ListMeta{TotalItems: filteredTotal}
 
 	for _, node := range nodes {
-		nodeList.Nodes = append(nodeList.Nodes, toNode(node))
+		pods, err := getNodePods(client, node)
+		if err != nil {
+			log.Printf("Couldn't get pods of %s node\n", node.Name)
+		}
+
+		nodeList.Nodes = append(nodeList.Nodes, toNode(node, pods))
 	}
 
 	// this may be slow because heapster does not support all in one download for nodes.
@@ -100,11 +104,17 @@ func toNodeList(nodes []api.Node, dsQuery *dataselect.DataSelectQuery, heapsterC
 	return nodeList
 }
 
-func toNode(node api.Node) Node {
+func toNode(node api.Node, pods *api.PodList) Node {
+	allocatedResources, err := getNodeAllocatedResources(node, pods)
+	if err != nil {
+		log.Printf("Couldn't get allocated resources of %s node\n", node.Name)
+	}
+
 	return Node{
-		ObjectMeta: common.NewObjectMeta(node.ObjectMeta),
-		TypeMeta:   common.NewTypeMeta(common.ResourceKindNode),
-		Ready:      getNodeConditionStatus(node, api.NodeReady),
+		ObjectMeta:         common.NewObjectMeta(node.ObjectMeta),
+		TypeMeta:           common.NewTypeMeta(common.ResourceKindNode),
+		Ready:              getNodeConditionStatus(node, api.NodeReady),
+		AllocatedResources: allocatedResources,
 	}
 }
 
