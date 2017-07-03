84b818923f95d7b5d856317bd0ac8041f04c143d
riverzhang <rong.zhang@easystack.cn>
2017-07-03 17:10:12 +0800

    Add service events (#2073)
    add events in the details of the service
    the test code for add service events

--- src/app/backend/resource/service/detail.go
+++ src/app/backend/resource/service/detail.go
@@ -52,6 +52,9 @@ type ServiceDetail struct {
 	// a valid IP address. None can be specified for headless services when proxying is not required
 	ClusterIP string `json:"clusterIP"`
 
+	// List of events related to this Service
+	EventList common.EventList `json:"eventList"`
+
 	// PodList represents list of pods targeted by same label selector as this service.
 	PodList pod.PodList `json:"podList"`
 }
@@ -73,8 +76,14 @@ func GetServiceDetail(client k8sClient.Interface, metricClient metricapi.MetricC
 		return nil, err
 	}
 
+	eventList, err := GetServiceEvents(client, dataselect.DefaultDataSelect, namespace, name)
+	if err != nil {
+		return nil, err
+	}
+
 	service := ToServiceDetail(serviceData)
 	service.PodList = *podList
+	service.EventList = *eventList
 
 	return &service, nil
 }
