900edfe9cf04d16351623db8c5395597af69fffb
rong.zhang <rong.zhang@easystack.cn>
2017-08-11 19:07:54 +0800

    Add secretType
    * Add secretType in secret list

--- src/app/backend/resource/secret/list.go
+++ src/app/backend/resource/secret/list.go
@@ -65,8 +65,9 @@ func (spec *ImagePullSecretSpec) GetData() map[string][]byte {
 
 // Secret is a single secret returned to the frontend.
 type Secret struct {
-	api.ObjectMeta `json:"objectMeta"`
-	api.TypeMeta   `json:"typeMeta"`
+	ObjectMeta    api.ObjectMeta  `json:"objectMeta"`
+	TypeMeta      api.TypeMeta    `json:"typeMeta"`
+	SecretType    v1.SecretType   `json:"secretType"`
 }
 
 // SecretsList is a response structure for a queried secrets list.
@@ -125,8 +126,9 @@ func CreateSecret(client *client.Clientset, spec SecretSpec) (*Secret, error) {
 
 func toSecret(secret *v1.Secret) *Secret {
 	return &Secret{
-		api.NewObjectMeta(secret.ObjectMeta),
-		api.NewTypeMeta(api.ResourceKindSecret),
+		ObjectMeta:    api.NewObjectMeta(secret.ObjectMeta),
+		TypeMeta:      api.NewTypeMeta(api.ResourceKindSecret),
+		SecretType:    secret.Type,
 	}
 }
 
