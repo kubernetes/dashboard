4c42100ed1eabfe79f64e6148e029f3046b6a932
Marcin Maciaszczyk <marcin.maciaszczyk@ts.fujitsu.com>
2017-10-12 10:38:02 +0200

    Use kubeadm

--- build/hyperkube.sh
+++ build/hyperkube.sh
@@ -13,23 +13,34 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# TODO Base directory that needs to be cached.
+BASE_DIR=/tmp
+
+# Architecture.
 ARCH=amd64
-BASE_DIR=/tmp # TODO Cache.
+
+# Binaries versions.
 ETCD_VERSION=v3.2.9
-ETCD_DIR=${BASE_DIR}/etcd
 K8S_VERSION=$(curl -sSL https://dl.k8s.io/release/stable.txt)
+
+# Binaries locations.
 KUBECTL_BIN=${BASE_DIR}/kubectl
 KUBEADM_BIN=${BASE_DIR}/kubeadm
 
 set -x
 
+
+
+
+
 # Download etcd.
+ETCD_DIR=${BASE_DIR}/etcd
 mkdir -p ${ETCD_DIR}
 curl -L https://github.com/coreos/etcd/releases/download/${ETCD_VERSION}/etcd-${ETCD_VERSION}-linux-amd64.tar.gz -o /tmp/etcd-${ETCD_VERSION}-linux-amd64.tar.gz
 tar xzvf /tmp/etcd-${ETCD_VERSION}-linux-amd64.tar.gz -C ${ETCD_DIR} --strip-components=1
 
 # Install cfssl.
-go get -u github.com/cloudflare/cfssl/cmd/...
+#go get -u github.com/cloudflare/cfssl/cmd/...
 
 # Download and setup kubectl.
 curl -L https://storage.googleapis.com/kubernetes-release/release/${K8S_VERSION}/bin/linux/${ARCH}/kubectl -o ${KUBECTL_BIN}
@@ -41,7 +52,7 @@ ${KUBECTL_BIN} config use-context local
 
 # Download and setup kubeadm.
 curl -sSL https://dl.k8s.io/release/${K8S_VERSION}/bin/linux/${ARCH}/kubeadm > ${KUBEADM_BIN}
-chmod a+rx ${KUBECTL_BIN}
+sudo chmod a+rx ${KUBEADM_BIN}
 
 ${KUBEADM_BIN} version
 # TODO
