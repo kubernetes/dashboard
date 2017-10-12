0ef6d9ec50d83127cc477d9d90afd107cf9397fc
Marcin Maciaszczyk <marcin.maciaszczyk@ts.fujitsu.com>
2017-10-12 13:24:17 +0200

    Test

--- build/hyperkube.sh
+++ build/hyperkube.sh
@@ -14,52 +14,54 @@
 # limitations under the License.
 
 # TODO Base directory that needs to be cached.
-BASE_DIR=/tmp
+#BASE_DIR=/tmp
 
-# Architecture.
-ARCH=amd64
+# TODO
+#ARCH=amd64
+#K8S_VERSION=$(curl -sSL https://dl.k8s.io/release/stable.txt)
+#KUBECTL_BIN=${BASE_DIR}/kubectl
 
-# Versions.
-ETCD_VERSION=v3.2.9
-K8S_VERSION=$(curl -sSL https://dl.k8s.io/release/stable.txt)
+# Download and setup kubectl.
+#curl -L https://storage.googleapis.com/kubernetes-release/release/${K8S_VERSION}/bin/linux/${ARCH}/kubectl -o ${KUBECTL_BIN}
+#sudo chmod +x ${KUBECTL_BIN}
+
 
 # Binaries locations.
-KUBECTL_BIN=${BASE_DIR}/kubectl
-KUBEADM_BIN=${BASE_DIR}/kubeadm
+#KUBEADM_BIN=${BASE_DIR}/kubeadm
 
 set -x
 
+curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 && chmod +x minikube
+curl -Lo kubectl https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl && chmod +x kubectl
+
+export MINIKUBE_WANTUPDATENOTIFICATION=false
+export MINIKUBE_WANTREPORTERRORPROMPT=false
+export MINIKUBE_HOME=$HOME
+export CHANGE_MINIKUBE_NONE_USER=true
+mkdir $HOME/.kube || true
+touch $HOME/.kube/config
+
+export KUBECONFIG=$HOME/.kube/config
+sudo -E ./minikube start --vm-driver=none
+
+# this for loop waits until kubectl can access the api server that Minikube has created
+for i in {1..150} # timeout for 5 minutes
+do
+   ./kubectl get po &> /dev/null
+   if [ $? -ne 1 ]; then
+      break
+  fi
+  sleep 2
+done
+
+# kubectl commands are now able to interact with Minikube cluster
 
 
 
 
-# Download etcd.
-ETCD_DIR=${BASE_DIR}/etcd
-mkdir -p ${ETCD_DIR}
-curl -L https://github.com/coreos/etcd/releases/download/${ETCD_VERSION}/etcd-${ETCD_VERSION}-linux-amd64.tar.gz -o /tmp/etcd-${ETCD_VERSION}-linux-amd64.tar.gz
-tar xzvf /tmp/etcd-${ETCD_VERSION}-linux-amd64.tar.gz -C ${ETCD_DIR} --strip-components=1
 
-# Install cfssl.
-#go get -u github.com/cloudflare/cfssl/cmd/...
 
-# Download and setup kubectl.
-curl -L https://storage.googleapis.com/kubernetes-release/release/${K8S_VERSION}/bin/linux/${ARCH}/kubectl -o ${KUBECTL_BIN}
-sudo chmod +x ${KUBECTL_BIN}
-${KUBECTL_BIN} config set-credentials myself --username=admin --password=admin
-${KUBECTL_BIN} config set-context local --cluster=local --user=myself
-${KUBECTL_BIN} config set-cluster local --server=http://localhost:8080
-${KUBECTL_BIN} config use-context local
 
-# Download and setup kubeadm.
-curl -sSL https://dl.k8s.io/release/${K8S_VERSION}/bin/linux/${ARCH}/kubeadm > ${KUBEADM_BIN}
-sudo chmod a+rx ${KUBEADM_BIN}
 
-${KUBEADM_BIN} version
-
-sudo ${KUBEADM_BIN} init --pod-network-cidr=10.244.0.0/16
-
-sudo cp /etc/kubernetes/admin.conf ${HOME}/.kube/config
-
-${KUBECTL_BIN} taint nodes --all node-role.kubernetes.io/master-
 
 # TODO
