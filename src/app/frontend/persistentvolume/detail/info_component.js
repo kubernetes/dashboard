71f08e35947feeace1b60ff4ce46deafecb16da9
Harisanker <hari.sanker@thinkcoretech.com>
2017-10-25 17:21:15 +0530

    Added claim links in PV details page

--- src/app/frontend/persistentvolume/detail/info_component.js
+++ src/app/frontend/persistentvolume/detail/info_component.js
@@ -12,20 +12,43 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+import {StateParams} from '../../common/resource/resourcedetail';
+import {stateName} from '../../persistentvolumeclaim/detail/state';
+
 /**
  * @final
  */
 export default class PersistentVolumeInfoController {
   /**
    * Constructs statefultion controller info object.
+   * @param {!ui.router.$state} $state
    * @ngInject
    */
-  constructor() {
+  constructor($state) {
     /**
      * Persistent volume details. Initialized from the scope.
      * @export {!backendApi.PersistentVolumeDetail}
      */
     this.persistentVolume;
+
+    /** @private {!ui.router.$state} */
+    this.state_ = $state;
+  }
+
+  /**
+   * Returns link to persistentvolumeclaim details page.
+   * @return {string}
+   * @export
+   */
+  getPersistentVolumeClaimDetailsHref() {
+    if(this.persistentVolume.claim) {
+      let claim = this.persistentVolume.claim.split('/')
+      if(claim.length >= 2) {
+        let namespace = claim[0]
+        let claimName = claim[1]
+        return this.state_.href(stateName, new StateParams(namespace, claimName));
+      }
+    }
   }
 }
 
