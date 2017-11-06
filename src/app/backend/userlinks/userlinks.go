1482eb95f86d536ca7bc7c59b2e0880317012036
Harisanker <hari.sanker@thinkcoretech.com>
2017-11-06 12:07:20 +0530

    Support for User Links add through annotations for PersistentVolumes

--- src/app/backend/userlinks/userlinks.go
+++ src/app/backend/userlinks/userlinks.go
@@ -31,6 +31,7 @@ const (
 	apiserverProxyURL = "http://{{apiserver-proxy-url}}"
 	podDNSName        = "http://{{pod.dns_name}}"
 	svcDNSName        = "http://{{svc.dns_name}}"
+	pvDNSName         = "http://{{pv.dns_name}}"
 )
 
 // UserLink is an optional annotation attached to the pod or service resource objects.
@@ -133,6 +134,41 @@ func getPodLinks(client k8sClient.Interface, namespace, name, host string) ([]Us
 		} else if strings.Contains(uri, podDNSName) {
 			//10-192-3-8.default.pod.cluster.localhttp://{{pod.dns_name}}:9090/debug
 			userLink.Link = strings.Replace(pod.Status.PodIP, ".", "-", -1) + "." + namespace + ".pod.cluster.local" + strings.TrimPrefix(uri, podDNSName)
+			userLink.IsURLValid = true
+		} else if _, err := url.ParseRequestURI(uri); err != nil {
+			userLink.Link = "Invalid User Link: " + uri
+		} else {
+			userLink.Link = uri
+			userLink.IsURLValid = true
+		}
+		userLinks = append(userLinks, *userLink)
+	}
+	return userLinks, err
+}
+
+// getPersistentvolumeLinks get userlinks for persistentvolume
+func getPersistentVolumeLinks(client k8sClient.Interface, name, host string) ([]UserLink, error) {
+	userLinks := []UserLink{}
+	persistentVolume, err := client.CoreV1().PersistentVolumes().Get(name, metaV1.GetOptions{})
+	if err != nil || len(persistentVolume.Annotations[annotationObj]) == 0 {
+		return userLinks, err
+	}
+
+	m := map[string]string{}
+	err = json.Unmarshal([]byte(persistentVolume.Annotations[annotationObj]), &m)
+	if err != nil {
+		return userLinks, err
+	}
+
+	for key, uri := range m {
+		userLink := new(UserLink)
+		userLink.Description = key
+		if strings.Contains(uri, apiserverProxyURL) {
+			userLink.Link = host + "/api/v1/persistentvolumes/" + persistentVolume.ObjectMeta.Name + "/proxy" + strings.TrimPrefix(uri, apiserverProxyURL)
+			userLink.IsURLValid = true
+			userLink.IsProxyURL = true
+		} else if strings.Contains(uri, pvDNSName) {
+			userLink.Link = name + ".pv.cluster.local" + strings.TrimPrefix(uri, pvDNSName)
 			userLink.IsURLValid = true
 		} else if _, err := url.ParseRequestURI(uri); err != nil {
 			userLink.Link = "Invalid User Link: " + uri
