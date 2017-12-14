0040db930f0ddddf702cdb7987073a527cfd6599
Li Yi <denverdino@gmail.com>
2017-12-14 14:19:01 +0800

    Format test case
    Signed-off-by: Li Yi <denverdino@gmail.com>

--- src/test/frontend/common/filters/relativetime_filter_test.js
+++ src/test/frontend/common/filters/relativetime_filter_test.js
@@ -217,15 +217,14 @@ describe('Relative time filter', () => {
        expect(relativeTime).toEqual('11 months');
      });
 
-  it(`should return 'didn't happen yet' string if given time is null`,
-     () => {
-       // given
-       // null
-       
-       // when
-       let relativeTime = relativeTimeFilter(null);
+  it(`should return 'didn't happen yet' string if given time is null`, () => {
+    // given
+    // null
 
-       // then
-       expect(relativeTime).toEqual(`-`);
-     });
+    // when
+    let relativeTime = relativeTimeFilter(null);
+
+    // then
+    expect(relativeTime).toEqual(`-`);
+  });
 });
