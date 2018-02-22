dd392dcec2a641c7d7bd984c5087b5a8cc623f24
zhangjianweibj <zhangjianweibj@inspur.com>
2018-02-22 14:13:30 +0800

    networkPolicy test case

--- src/app/backend/resource/networkpolicy/detail_test.go
+++ src/app/backend/resource/networkpolicy/detail_test.go
@@ -19,7 +19,6 @@ import (
   "testing"
 
   "github.com/kubernetes/dashboard/src/app/backend/api"
-  "github.com/kubernetes/dashboard/src/app/backend/resource/dataselect"
   "github.com/kubernetes/dashboard/src/app/backend/resource/common"
   core "k8s.io/api/core/v1"
   networkpolicy "k8s.io/api/networking/v1"
