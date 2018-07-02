f223aa85e37af5743527c9e645dcfa57190b7174
Arslanbekov Denis <Arslanbekov@gmail.com>
2018-07-02 11:50:06 +0300

    Support spec variable in detail pod page. (#2942)
    * On the detailed page (containerinfo) added the display of variables from spec
    
    * CI error fix
    
    * Unit test for function getFieldRefValue()
    
    * Fix concatenation
    
    * Remove space

--- src/app/externs/backendapi.js
+++ src/app/externs/backendapi.js
@@ -775,7 +775,8 @@ backendApi.EnvVar;
 /**
  * @typedef {{
  *   configMapKeyRef: backendApi.ConfigMapKeyRef,
- *   secretKeyRef: backendApi.SecretKeyRef
+ *   secretKeyRef: backendApi.SecretKeyRef,
+ *   fieldRef: backendApi.fieldRef
  * }}
  */
 backendApi.EnvVarSource;
@@ -795,6 +796,14 @@ backendApi.ConfigMapKeyRef;
  * }}
  */
 backendApi.SecretKeyRef;
+
+/**
+ * @typedef {{
+ *   apiVersion: string,
+ *   fieldPath: string,
+ * }}
+ */
+backendApi.fieldRef;
 
 /**
  * @typedef {{
