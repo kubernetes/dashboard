9b28b1985ff35663735d84087a68264f46b81ab9
Robertus Johansyah <kororo@users.noreply.github.com>
2018-08-30 06:29:51 +1000

    Test Implementation for ng-migration (#3212)
    * Update package versions
    Update ng command test for Angular 6
    
    * Ignore go vet check
    Ignore discovery frontend test
    
    * Add AboutComponent test
    
    * Format the code and sign the CLA
    
    * Enable back run-go-vet
    Fix typo in package.json
    
    * Merge code with https://github.com/kubernetes/dashboard/pull/3223

--- src/app/frontend/about/component.spec.ts
+++ src/app/frontend/about/component.spec.ts
@@ -12,10 +12,79 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+import {HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+import {By} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {AppConfig} from '@api/backendapi';
+import {SharedModule} from 'shared.module';
+import {CardComponent} from '../common/components/card/component';
+import {AssetsService} from '../common/services/global/assets';
+import {ConfigService} from '../common/services/global/config';
 import {AboutComponent} from './component';
 
 describe('AboutComponent', () => {
-  it('should pass', () => {
-    expect(true).toBeTruthy();
-  });
+  let component: AboutComponent;
+  let fixture: ComponentFixture<AboutComponent>;
+  let httpMock: HttpTestingController;
+  let configService: ConfigService;
+  let element: HTMLElement;
+
+  // set the predefined values
+  const commit = 'AA7A6A1ADC218BBC09FEDB653C9A9432F7EF9903';
+  const version = '88';
+  const copyrightYear = 2018;
+
+  beforeEach(async(() => {
+    TestBed
+        .configureTestingModule({
+          imports: [SharedModule, HttpClientTestingModule, BrowserAnimationsModule],
+          declarations: [AboutComponent, CardComponent],
+          providers: [AssetsService, ConfigService],
+        })
+        .compileComponents();
+    httpMock = TestBed.get(HttpTestingController);
+    configService = TestBed.get(ConfigService);
+  }));
+
+  beforeEach(async(() => {
+    // prepare the component
+    configService.init();
+    fixture = TestBed.createComponent(AboutComponent);
+    component = fixture.componentInstance;
+
+    const configRequest = httpMock.expectOne('config');
+    const config: AppConfig = {serverTime: new Date().getTime()};
+    configRequest.flush(config);
+
+    // set the fixed values
+    component.appVersion = version;
+    component.gitCommit = commit;
+    component.latestCopyrightYear = copyrightYear;
+
+    // grab the HTML element
+    element = fixture.debugElement.query(By.css('kd-card')).nativeElement;
+  }));
+
+  it('should create github link', async(() => {
+       fixture.detectChanges();
+       const url = component.getCommitLink();
+       expect(url.indexOf('https://github.com') === 0).toBeTruthy();
+       expect(url.indexOf(commit) > 0).toBeTruthy();
+     }));
+
+  it('should print app version', async(() => {
+       fixture.detectChanges();
+       expect(element.textContent).toContain(`${version}`);
+     }));
+
+  it('should print git commit', async(() => {
+       fixture.detectChanges();
+       expect(element.textContent).toContain(commit);
+     }));
+
+  it('should print current year', async(() => {
+       fixture.detectChanges();
+       expect(element.textContent).toContain(`2015 - ${copyrightYear}`);
+     }));
 });
