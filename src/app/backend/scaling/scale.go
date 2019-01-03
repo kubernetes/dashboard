68baf08f2f49c0bcb51286f83ea8ab40e1c94b11
Marcin Maciaszczyk <maciaszczykm@icloud.com>
2019-01-03 22:34:17 +0100

    Rewrite scaling to use polymorphic scale client

--- src/app/backend/scaling/scale.go
+++ src/app/backend/scaling/scale.go
@@ -15,18 +15,14 @@
 package scaling
 
 import (
-	"k8s.io/client-go/restmapper"
 	"strconv"
-	"strings"
 
-	"k8s.io/api/extensions/v1beta1"
-	metaV1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/discovery"
-	cacheddiscovery "k8s.io/client-go/discovery/cached"
+	"k8s.io/client-go/discovery/cached"
 	"k8s.io/client-go/dynamic"
-	client "k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/kubernetes/scheme"
 	"k8s.io/client-go/rest"
+	"k8s.io/client-go/restmapper"
 	"k8s.io/client-go/scale"
 	"k8s.io/client-go/scale/scheme/appsv1beta2"
 )
@@ -39,23 +35,10 @@ type ReplicaCounts struct {
 
 // GetScaleSpec returns a populated ReplicaCounts object with desired and actual number of replicas.
 func GetScaleSpec(cfg *rest.Config, kind, namespace, name string) (*ReplicaCounts, error) {
-	discoveryClient, err := discovery.NewDiscoveryClientForConfig(cfg)
+	sc, err := getScaleGetter(cfg)
 	if err != nil {
 		return nil, err
 	}
-
-	cfg.GroupVersion = &appsv1beta2.SchemeGroupVersion
-	cfg.NegotiatedSerializer = scheme.Codecs
-
-	restClient, err := rest.RESTClientFor(cfg)
-	if err != nil {
-		return nil, err
-	}
-
-	resolver := scale.NewDiscoveryScaleKindResolver(discoveryClient)
-	dc := cacheddiscovery.NewMemCacheClient(discoveryClient)
-	drm := restmapper.NewDeferredDiscoveryRESTMapper(dc)
-	sc := scale.New(restClient, drm, dynamic.LegacyAPIPathResolverFunc, resolver)
 
 	res, err := sc.Scales(namespace).Get(appsv1beta2.Resource(kind), name)
 	if err != nil {
@@ -71,92 +54,56 @@ func GetScaleSpec(cfg *rest.Config, kind, namespace, name string) (*ReplicaCount
 // ScaleResource scales the provided resource using the client scale method in the case of Deployment,
 // ReplicaSet, Replication Controller. In the case of a job we are using the jobs resource update
 // method since the client scale method does not provide one for the job.
-func ScaleResource(cfg *rest.Config, client client.Interface, kind, namespace, name, count string) (rc *ReplicaCounts, err error) {
-	rc = new(ReplicaCounts)
-	if strings.ToLower(kind) == "job" {
-		err = scaleJobResource(client, namespace, name, count, rc)
-	} else if strings.ToLower(kind) == "statefulset" {
-		err = scaleStatefulSetResource(client, namespace, name, count, rc)
-	} else {
-		discoveryClient, err := discovery.NewDiscoveryClientForConfig(cfg)
-		if err != nil {
-			return nil, err
-		}
-		err = scaleGenericResource(discoveryClient, client, kind, namespace, name, count)
-	}
+func ScaleResource(cfg *rest.Config, kind, namespace, name, count string) (rc *ReplicaCounts, err error) {
+	sc, err := getScaleGetter(cfg)
 	if err != nil {
 		return nil, err
 	}
 
-	return
-}
-
-//ScaleGenericResource is used for Deployment, ReplicaSet, Replication Controller scaling.
-func scaleGenericResource(discoveryClient *discovery.DiscoveryClient, client client.Interface, kind, namespace, name, count string) error {
-	result := &v1beta1.Scale{}
-
-	err := discoveryClient.RESTClient().Get().Namespace(namespace).Resource(kind+"s").Name(name).
-		SubResource("scale").VersionedParams(&metaV1.GetOptions{}, scheme.ParameterCodec).Do().Into(result)
+	res, err := sc.Scales(namespace).Get(appsv1beta2.Resource(kind), name)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
 	c, err := strconv.Atoi(count)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
-	// Update replicas count.
-	result.Spec.Replicas = int32(c)
+	res.Spec.Replicas = int32(c)
 
-	return client.Discovery().RESTClient().Put().Namespace(namespace).Resource(kind + "s").Name(name).
-		SubResource("scale").Body(result).Do().Into(result)
+	res, err = sc.Scales(namespace).Update(appsv1beta2.Resource(kind), res)
+	if err != nil {
+		return nil, err
+	}
+
+	return &ReplicaCounts{
+		ActualReplicas:  res.Status.Replicas,
+		DesiredReplicas: res.Spec.Replicas,
+	}, nil
 }
 
-// scaleJobResource is exclusively used for jobs as it does not increase/decrease pods but jobs parallelism attribute.
-func scaleJobResource(client client.Interface, namespace, name, count string, rc *ReplicaCounts) error {
-	j, err := client.BatchV1().Jobs(namespace).Get(name, metaV1.GetOptions{})
+func getScaleGetter(cfg *rest.Config) (scale.ScalesGetter, error) {
+	discoveryClient, err := discovery.NewDiscoveryClientForConfig(cfg)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
-	c, err := strconv.Atoi(count)
+	cfg.GroupVersion = &appsv1beta2.SchemeGroupVersion
+	cfg.NegotiatedSerializer = scheme.Codecs
+
+	restClient, err := rest.RESTClientFor(cfg)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
-	*j.Spec.Parallelism = int32(c)
-	j, err = client.BatchV1().Jobs(namespace).Update(j)
-	if err != nil {
-		return err
-	}
+	resolver := scale.NewDiscoveryScaleKindResolver(discoveryClient)
+	dc := cached.NewMemCacheClient(discoveryClient)
+	drm := restmapper.NewDeferredDiscoveryRESTMapper(dc)
 
-	rc.DesiredReplicas = *j.Spec.Parallelism
-	rc.ActualReplicas = *j.Spec.Parallelism
+	// Fixes "unable to get full preferred group-version-resource for <resource>: the cache has not been filled yet".
+	// See more: https://github.com/kubernetes/kubernetes/issues/68735
+	drm.Reset()
 
-	return nil
-}
-
-// scaleStatefulSet is exclusively used for stateful sets.
-func scaleStatefulSetResource(client client.Interface, namespace, name, count string, rc *ReplicaCounts) error {
-	ss, err := client.AppsV1().StatefulSets(namespace).Get(name, metaV1.GetOptions{})
-	if err != nil {
-		return err
-	}
-
-	c, err := strconv.Atoi(count)
-	if err != nil {
-		return err
-	}
-
-	*ss.Spec.Replicas = int32(c)
-	ss, err = client.AppsV1().StatefulSets(namespace).Update(ss)
-	if err != nil {
-		return err
-	}
-
-	rc.DesiredReplicas = *ss.Spec.Replicas
-	rc.ActualReplicas = ss.Status.Replicas
-
-	return nil
+	return scale.New(restClient, drm, dynamic.LegacyAPIPathResolverFunc, resolver), nil
 }
