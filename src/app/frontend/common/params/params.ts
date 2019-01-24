ea4e02aa7d4be91afebf7fd7e2a06e98a886ba74
Sebastian Florek <s.florek91@gmail.com>
2019-01-24 12:35:31 +0100

    Disable skip button based on backend (#3533)

--- src/app/frontend/common/params/params.ts
+++ src/app/frontend/common/params/params.ts
@@ -12,7 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import {K8sError} from '@api/backendapi';
 import {KdError} from '@api/frontendapi';
 import {StateParams} from '@uirouter/core';
 
