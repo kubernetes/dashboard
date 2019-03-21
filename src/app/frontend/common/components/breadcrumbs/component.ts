03ae5c5d0ec447a31f2db26e141b2068d4e347f1
Marcin Maciaszczyk <maciaszczykm@icloud.com>
2019-03-21 12:49:42 +0100

    Display current state in the breadcrumbs

--- src/app/frontend/common/components/breadcrumbs/component.ts
+++ src/app/frontend/common/components/breadcrumbs/component.ts
@@ -12,70 +12,65 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute, ActivatedRouteSnapshot, NavigationEnd, Router} from '@angular/router';
 import {Breadcrumb} from '@api/frontendapi';
-import {StateDeclaration, StateObject, StateService, TransitionService} from '@uirouter/core';
 import {BreadcrumbsService} from '../../services/global/breadcrumbs';
 
-/**
- * Should be used only within actionbar component.
- *
- * In order to define custom label for the state add: `'kdBreadcrumbs':{'label':'myLabel'}`
- * to the state config. This label will be used instead of default state name when displaying
- * breadcrumbs.
- *
- * In order to define custom parent for the state add: `'kdBreadcrumbs`:{'parent':'myParentState'}`
- * to the state config. Parent state will be looked up by this name if it's defined.
- *
- * Additionally labels can be interpolated. This applies only to the last state in the
- * breadcrumb chain, i.e. for given state chain `StateA > StateB > StateC`, only StateC can use
- * following convention: `'kdBreadcrumbs`:{'label':'paramX'}`. Note that 'paramX' has to be a part
- * of state params object to be correctly interpolated.
- *
- * Example state config:
- * $stateProvider.state(stateName, {
- *   url: '...',
- *   data: {
- *      'kdBreadcrumbs': {
- *        'label': 'paramX',
- *        'parent': 'parentState',
- *      },
- *   },
- *   views: {
- *     '': {
- *       controller: SomeCtrlA,
- *       controllerAs: 'ctrl',
- *       templateUrl: '...',
- *     },
- *     'actionbar': {
- *       controller: SomeCtrlB,
- *       controllerAs: 'ctrl',
- *       templateUrl: '...',
- *     },
- *   },
- * });
- */
 @Component({
   selector: 'kd-breadcrumbs',
   templateUrl: './template.html',
   styleUrls: ['./style.scss'],
 })
 export class BreadcrumbsComponent implements OnInit {
-  @Input() limit: number;
-  breadcrumbs: Breadcrumb[];
-  constructor(private readonly router_: Router, private readonly breadcrumbs_: BreadcrumbsService) {
-  }
+  breadcrumbs: Breadcrumb[] = [];
+
+  constructor(
+      private readonly _router: Router, private readonly _activatedRoute: ActivatedRoute,
+      private readonly _breadcrumbs: BreadcrumbsService) {}
 
   ngOnInit(): void {
-    this.initBreadcrumbs_();
-    // this.transition_.onSuccess({}, () => {
-    //   this.initBreadcrumbs_();
-    // });
+    this._registerNavigationHook();
   }
 
-  /** Initializes breadcrumbs array by traversing states parents until none is found. */
-  initBreadcrumbs_(): void {
+  private _registerNavigationHook(): void {
+    this._router.events.filter(event => event instanceof NavigationEnd)
+        .distinctUntilChanged()
+        .subscribe(() => {
+          this._initBreadcrumbs();
+        });
+  }
+
+  private _initBreadcrumbs(): void {
+    this.breadcrumbs = [];
+
+    const currentRoute = this._getCurrentRoute();
+
+    // TODO
+    this.breadcrumbs.push({
+      label: currentRoute.routeConfig.component.name,
+      stateLink: '',
+    });
+
+    // const label = route.routeConfig && route.routeConfig.data ? route.routeConfig.data[
+    //   'breadcrumb' ] : 'Home'; const nextUrl = `${url}${route.routeConfig ?
+    //   route.routeConfig.path :
+    //   ''}/`; breadcrumbs.push({label: label, stateLink: nextUrl}); return route.firstChild ?
+    //   this._buildBreadCrumb(route.firstChild, nextUrl, breadcrumbs) : breadcrumbs;
+
+
+
+    // _buildBreadCrumb(route: ActivatedRoute, url: string = '', breadcrumbs: Breadcrumb[] = []):
+    // Breadcrumb[] {
+    //   const label = route.routeConfig && route.routeConfig.data ? route.routeConfig.data[
+    //   'breadcrumb' ] : 'Home'; const nextUrl = `${url}${route.routeConfig ?
+    //   route.routeConfig.path :
+    //   ''}/`; breadcrumbs.push({label: label, stateLink: nextUrl}); return route.firstChild ?
+    //   this._buildBreadCrumb(route.firstChild, nextUrl, breadcrumbs) : breadcrumbs;
+    // }
+
+
+
     // let state: ActivatedRouteSnapshot = this.router_.routerState.root.snapshot;
     // const breadcrumbs: Breadcrumb[] = [];
     //
@@ -92,12 +87,12 @@ export class BreadcrumbsComponent implements OnInit {
     // this.breadcrumbs = breadcrumbs.reverse();
   }
 
-  /**
-   * Returns true if limit is undefined or limit is defined and breadcrumbs count is smaller or
-   * equal to the limit.
-   */
-  private canAddBreadcrumb_(breadcrumbs: Breadcrumb[]): boolean {
-    return this.limit === undefined || this.limit > breadcrumbs.length;
+  private _getCurrentRoute(): ActivatedRoute {
+    let route = this._activatedRoute.root;
+    while (route && route.firstChild) {
+      route = route.firstChild;
+    }
+    return route;
   }
 
   private getBreadcrumb_(state: ActivatedRouteSnapshot): Breadcrumb {
@@ -111,19 +106,3 @@ export class BreadcrumbsComponent implements OnInit {
     return breadcrumb;
   }
 }
-
-// breadcrumbs = this._router.events
-//   .filter(event => event instanceof NavigationEnd)
-//   .distinctUntilChanged()
-//   .map(() => this._buildBreadCrumb(this._activatedRoute.root));
-//
-// constructor(private readonly _router: Router, private readonly _activatedRoute: ActivatedRoute)
-// {}
-//
-// _buildBreadCrumb(route: ActivatedRoute, url: string = '', breadcrumbs: Breadcrumb[] = []):
-// Breadcrumb[] {
-//   const label = route.routeConfig && route.routeConfig.data ? route.routeConfig.data[
-//   'breadcrumb' ] : 'Home'; const nextUrl = `${url}${route.routeConfig ? route.routeConfig.path :
-//   ''}/`; breadcrumbs.push({label: label, stateLink: nextUrl}); return route.firstChild ?
-//   this._buildBreadCrumb(route.firstChild, nextUrl, breadcrumbs) : breadcrumbs;
-// }
