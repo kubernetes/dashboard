103c157ed6e80a2ad03fe55f66d2c86d38e87db1
Elijah Oyekunle <eloyekunle@gmail.com>
2019-05-19 18:27:14 +0100

    remove nvd3 and install c3

--- src/app/frontend/common/components/allocationchart/component.ts
+++ src/app/frontend/common/components/allocationchart/component.ts
@@ -12,11 +12,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import { Component, Input, OnInit } from '@angular/core';
-import * as d3 from 'd3';
-import { Selection } from 'd3';
-import * as nv from 'nvd3';
-import { Nvd3Element } from 'nvd3';
+import { AfterViewInit, Component, Input } from '@angular/core';
+import { Selection, select } from 'd3';
+import { generate, ChartAPI } from 'c3';
 
 interface PieChart {
   data: PieChartData;
@@ -24,13 +22,14 @@ interface PieChart {
 
 interface PieChartData {
   value: number;
+  color: string;
 }
 
 @Component({
   selector: 'kd-allocation-chart',
   templateUrl: './template.html',
 })
-export class AllocationChartComponent implements OnInit {
+export class AllocationChartComponent implements AfterViewInit {
   @Input() data: PieChartData[];
   @Input() colorPalette: string[];
   @Input() outerPercent: number;
@@ -42,112 +41,84 @@ export class AllocationChartComponent implements OnInit {
   @Input() size: number;
   @Input() id: string;
 
-  ngOnInit(): void {
-    this.generateGraph_();
+  ngAfterViewInit(): void {
+    setTimeout(() => this.generateGraph_(), 0);
   }
 
-  /**
-   * Initializes pie chart graph. Check documentation at:
-   * https://nvd3-community.github.io/nvd3/examples/documentation.html#pieChart
-   */
   initPieChart_(
-    svg: Selection<{}>,
     data: PieChartData[],
-    colors: string[],
-    margin: number,
+    padding: number,
     ratio: number,
     labelFunc: (d: {}, i: number, values: {}) => string | null
-  ): Nvd3Element {
+  ): ChartAPI {
     const size = this.size || 280;
 
     if (!labelFunc) {
       labelFunc = this.formatLabel_;
     }
 
-    const chart = nv.models
-      .pieChart()
-      .showLegend(false)
-      .showLabels(true)
-      .x(d => d.key)
-      .y(d => d.value)
-      .donut(true)
-      .donutRatio(ratio)
-      .color(colors)
-      .margin({ top: margin, right: margin, bottom: margin, left: margin })
-      .width(size)
-      .height(size)
-      .growOnHover(false)
-      .labelType(labelFunc);
+    const colors: { [key: string]: string } = {};
+    data.map(x => (colors[x.value] = x.color));
 
-    chart.tooltip.enabled(this.enableTooltips);
-    chart.tooltip.contentGenerator(obj => {
-      /**
-       * This is required because the RatioItem.key is of the form `label: count` and the
-       * RatioItem.value stores the percentage. This splits the key to extract label and
-       * the count.
-       */
-      if (obj.data.key.includes(':')) {
-        const values = obj.data.key.split(':');
-        return `<h3>${values[0]}</h3><p>${values[1]}</p>`;
-      }
-      return obj.data.key;
+    return generate({
+      bindto: `#${this.id}`,
+      size: {
+        width: size,
+        height: size,
+      },
+      legend: {
+        show: false,
+      },
+      tooltip: {
+        show: this.enableTooltips,
+      },
+      transition: { duration: 350 },
+      donut: {
+        width: size * (1 - ratio),
+      },
+      data: {
+        columns: data.map(x => [x.value, x.value]),
+        type: 'donut',
+        colors,
+      },
+      padding: { top: padding, right: padding, bottom: padding, left: padding },
     });
-
-    svg
-      .attr('height', size)
-      .attr('width', size)
-      .append('g')
-      .datum(data)
-      .transition()
-      .duration(350)
-      .call(chart);
-
-    return chart;
   }
 
   /**
    * Generates graph using provided requests and limits bindings.
    */
   generateGraph_(): void {
-    nv.addGraph(() => {
-      const svg = d3.select(`#${this.id}`).append('svg');
-      if (!this.data) {
-        let chart: Nvd3Element;
-        if (this.outerPercent !== undefined) {
-          this.outerColor = this.outerColor ? this.outerColor : '#00c752';
-          chart = this.initPieChart_(
-            svg,
-            [{ value: this.outerPercent }, { value: 100 - this.outerPercent }],
-            [this.outerColor, '#ddd'],
-            0,
-            0.67,
-            this.displayOnlyAllocated_
-          );
-        }
-        if (this.innerPercent !== undefined) {
-          this.innerColor = this.innerColor ? this.innerColor : '#326de6';
-          chart = this.initPieChart_(
-            svg,
-            [{ value: this.innerPercent }, { value: 100 - this.innerPercent }],
-            [this.innerColor, '#ddd'],
-            39,
-            0.55,
-            this.displayOnlyAllocated_
-          );
-        }
-        return chart;
-      } else {
-        // Initializes a pie chart with multiple entries in a single ring
-        return this.initPieChart_(
-          svg,
-          this.data,
-          this.colorPalette,
+    if (!this.data) {
+      if (this.outerPercent !== undefined) {
+        this.outerColor = this.outerColor ? this.outerColor : '#00c752';
+        this.initPieChart_(
+          [
+            { value: this.outerPercent, color: this.outerColor },
+            { value: 100 - this.outerPercent, color: '#ddd' },
+          ],
           0,
-          this.ratio,
-          null
+          0.67,
+          this.displayOnlyAllocated_
         );
       }
-    });
+
+      if (this.innerPercent !== undefined) {
+        this.innerColor = this.innerColor ? this.innerColor : '#326de6';
+        this.initPieChart_(
+          [
+            { value: this.innerPercent, color: this.innerColor },
+            { value: 100 - this.innerPercent, color: '#ddd' },
+          ],
+          39,
+          0.55,
+          this.displayOnlyAllocated_
+        );
+      }
+    } else {
+      // Initializes a pie chart with multiple entries in a single ring
+      this.initPieChart_(this.data, 0, this.ratio, null);
+    }
   }
 
   /**
