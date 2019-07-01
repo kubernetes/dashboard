34307cf0a24541b6795b41cf3f04da4ff939de6f
Elijah Oyekunle <eloyekunle@gmail.com>
2019-07-01 15:22:21 +0100

    add echo statements

--- aio/scripts/update-vendor.sh
+++ aio/scripts/update-vendor.sh
@@ -2,8 +2,10 @@
 
 TMP_DIR="${TMP_DIR:-$(mktemp -d /tmp/update-vendor.XXXX)}"
 
+echo "vendor: running 'go mod tidy'"
 go mod tidy -v
 
+echo "vendor: running 'go mod vendor'"
 go mod vendor
 
 # sort recorded packages for a given vendored dependency in modules.txt.
@@ -11,3 +13,5 @@ go mod vendor
 # scan                 | prefix comment lines with the module name       | sort field 1  | strip leading text on comment lines
 awk '{if($1=="#") print $2 " " $0; else print}' < vendor/modules.txt | sort -k1,1 -s | sed 's/.*#/#/' > "${TMP_DIR}/modules.txt.tmp"
 mv "${TMP_DIR}/modules.txt.tmp" vendor/modules.txt
+
+echo "Vendor Updated"
