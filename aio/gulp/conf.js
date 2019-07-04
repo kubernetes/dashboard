9f9bba272668b2d40cfbdf7e112024c4a2853b1f
Marcin Maciaszczyk <maciaszczykm@icloud.com>
2019-07-04 17:14:37 +0200

    Release v2.0.0-beta1 (#4018)
    * add scraper to recommended. remove amd64 tag (we have manifests now)
    
    * Update version to v2.0.0-beta1
    
    * Regenerate package-lock.json

--- aio/gulp/conf.js
+++ aio/gulp/conf.js
@@ -53,7 +53,7 @@ const version = {
   /**
    * Current release version of the project.
    */
-  release: 'v2.0.0-alpha0',
+  release: 'v2.0.0-beta1',
   /**
    * Version name of the head release of the project.
    */
@@ -61,7 +61,7 @@ const version = {
   /**
    * Year of last source change of the project
    */
-  year: '2018',
+  year: '2019',
 };
 
 /**
