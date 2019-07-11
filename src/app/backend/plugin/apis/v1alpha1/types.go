042a9b118dacaac77c679d8d774b9940fe221193
ajatprabha <ajat.prabha.leo@gmail.com>
2019-07-11 14:51:51 +0530

    add comments to files

--- src/app/backend/plugin/apis/v1alpha1/types.go
+++ src/app/backend/plugin/apis/v1alpha1/types.go
@@ -22,6 +22,7 @@ import (
 // +genclient
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
 
+// Plugin holds the object information for Plugin kind, it also implements runtime.Object
 type Plugin struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata,omitempty"`
@@ -29,10 +30,12 @@ type Plugin struct {
 	Spec PluginSpec `json:"spec"`
 }
 
+// PluginSpec holds the specs for the Plugin kind
 type PluginSpec struct {
 	Source Source `json:"source"`
 }
 
+// Source holds the information about the plugin's source code origin
 type Source struct {
 	Filename     string                     `json:"filename,omitempty"`
 	ConfigMapRef *coreV1.ConfigMapEnvSource `json:"configMapRef,omitempty" protobuf:"bytes,1,opt,name=configMapRef"`
@@ -40,6 +43,7 @@ type Source struct {
 
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
 
+// PluginList holds the list information for Plugin kind, it also implements runtime.Object
 type PluginList struct {
 	metav1.TypeMeta `json:",inline"`
 	metav1.ListMeta `json:"metadata"`
--- src/app/backend/plugin/list.go
+++ src/app/backend/plugin/list.go
@@ -1,3 +1,17 @@
+// Copyright 2017 The Kubernetes Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package plugin
 
 import (
@@ -8,18 +22,23 @@ import (
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
+// PluginList holds only necessary information and is used to
+// map v1alpha1.PluginList to plugin.PluginList
 type PluginList struct {
 	ListMeta api.ListMeta `json:"listMeta"`
 	Plugins  []Plugin     `json:"plugins"`
 	Errors   []error      `json:"errors"`
 }
 
+// PluginList holds only necessary information and is used to
+// map v1alpha1.Plugin to plugin.Plugin
 type Plugin struct {
 	Name         string   `json:"name"`
 	Path         string   `json:"path"`
 	Dependencies []string `json:"dependencies"`
 }
 
+// GetPluginList returns all the registered plugins
 func GetPluginList(client pluginclientset.Interface, ns string) (*PluginList, error) {
 	pluginList, err := client.DashboardV1alpha1().Plugins(ns).List(v1.ListOptions{})
 	if err != nil {
