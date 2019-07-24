eae0d819d1b83f100cf0d39a6d386db21df86172
Matt Kohl <mattkohl@users.noreply.github.com>
2019-07-24 08:46:16 +0100

    Update errors.ts (#4109)
    Fixed grammatical mistake in error msg

--- src/app/frontend/common/errors/errors.ts
+++ src/app/frontend/common/errors/errors.ts
@@ -35,7 +35,7 @@ export enum ErrorCode {
 }
 
 const localizedErrors: {[key: string]: string} = {
-  MSG_TOKEN_EXPIRED_ERROR: 'You have been logged out because your token have expired.',
+  MSG_TOKEN_EXPIRED_ERROR: 'You have been logged out because your token has expired.',
   MSG_ENCRYPTION_KEY_CHANGED: 'You have been logged out because your token is invalid.',
   MSG_ACCESS_DENIED: 'Access denied.',
   MSG_DASHBOARD_EXCLUSIVE_RESOURCE_ERROR: 'Trying to access/modify dashboard exclusive resource.',
