693eedcd1414fe007e5d96125d875ee0d09ffcd5
Anshul Ahuja <anshul.ahu@gmail.com>
2019-12-14 00:21:01 +0530

    Cleanup commands.ts

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -25,47 +25,16 @@
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
 
 // add a custom command cy.foo()
-// Cypress.Commands.add('foo', () => 'foo')
+Cypress.Commands.add('foo', () => 'foo')
 //
 // // see more example of adding custom commands to Cypress TS interface
 // // in https://github.com/cypress-io/add-cypress-custom-command-in-typescript
 // // add new command to the existing Cypress interface
 // // tslint:disable-next-line no-namespace
-// declare namespace Cypress {
-//   // tslint:disable-next-line interface-name
-//   interface Chainable {
-//     foo: () => string
-//   }
-// }
-
-Cypress.Commands.add(
-  'createResource',
-  ({resource = '', name = '', namespace = '', content = ''}) => {
-    return getCsrfToken('appdeploymentfromfile').then(response => {
-      const {token} = response.body;
-
-      return cy.request({
-        url: '/api/v1/appdeploymentfromfile',
-        method: 'POST',
-        headers: {
-          Accept: 'application/json',
-          'Content-Type': 'application/json',
-          'X-CSRF-TOKEN': token,
-        },
-        body: {
-          name,
-          namespace,
-          resource,
-          content,
-        },
-      });
-    });
-  },
-);
-
-function getCsrfToken(action) {
-  return cy.request({
-    url: `api/v1/csrftoken/${action}`,
-    method: 'GET',
-  });
+declare namespace Cypress {
+  // tslint:disable-next-line interface-name
+  interface Chainable {
+    foo: () => string
+  }
 }
+
