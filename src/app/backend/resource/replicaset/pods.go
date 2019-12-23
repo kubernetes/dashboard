116d55988dfde877f8b2615b01b9dd942a8751dd
zehuaiWANG <874697675@qq.com>
2019-12-23 09:25:26 +0800

    golangci fix

--- src/app/backend/resource/replicaset/pods.go
+++ src/app/backend/resource/replicaset/pods.go
@@ -17,17 +17,17 @@ package replicaset
 import (
 	"log"
 
-  "github.com/kubernetes/dashboard/src/app/backend/errors"
-  metricapi "github.com/kubernetes/dashboard/src/app/backend/integration/metric/api"
-  "github.com/kubernetes/dashboard/src/app/backend/resource/common"
-  "github.com/kubernetes/dashboard/src/app/backend/resource/dataselect"
-  "github.com/kubernetes/dashboard/src/app/backend/resource/event"
-  "github.com/kubernetes/dashboard/src/app/backend/resource/pod"
-  apps "k8s.io/api/apps/v1"
-  v1 "k8s.io/api/core/v1"
-  metaV1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-  "k8s.io/apimachinery/pkg/labels"
-  k8sClient "k8s.io/client-go/kubernetes"
+	"github.com/kubernetes/dashboard/src/app/backend/errors"
+	metricapi "github.com/kubernetes/dashboard/src/app/backend/integration/metric/api"
+	"github.com/kubernetes/dashboard/src/app/backend/resource/common"
+	"github.com/kubernetes/dashboard/src/app/backend/resource/dataselect"
+	"github.com/kubernetes/dashboard/src/app/backend/resource/event"
+	"github.com/kubernetes/dashboard/src/app/backend/resource/pod"
+	apps "k8s.io/api/apps/v1"
+	v1 "k8s.io/api/core/v1"
+	metaV1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/labels"
+	k8sClient "k8s.io/client-go/kubernetes"
 )
 
 // GetReplicaSetPods return list of pods targeting replica set.
