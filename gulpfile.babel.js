b5825c167389dfb3b6d81c252d8aeea14797fb60
Shu Muto <shu-mutou@rf.jp.nec.com>
2020-01-15 04:39:12 +0000

    Change license check packages
    Use license-check-and-add instead gulp-header-license and gulp-license-check.

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,7 +1,6 @@
 // Root configuration of the gulp.js build system, loads child modules which define specific tasks.
 // Read more at: https://gulpjs.com/
 
-import './aio/gulp/check';
 import './aio/gulp/backend';
 import './aio/gulp/serve';
 import './aio/gulp/deploy';
