1ce7fa10fd090ff9c0a077a5d178499048487719
Sebastian Florek <Sebastian.Florek@loodse.com>
2020-01-16 14:42:22 +0100

    Bump Dashboard to v2.0.0-rc2 (#4794)

--- aio/gulp/conf.js
+++ aio/gulp/conf.js
@@ -53,7 +53,7 @@ const version = {
   /**
    * Current release version of the project.
    */
-  release: 'v2.0.0-rc1',
+  release: 'v2.0.0-rc2',
   /**
    * Version name of the head release of the project.
    */
