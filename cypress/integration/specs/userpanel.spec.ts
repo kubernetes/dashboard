65d0ed7a726a5d6794f0db619a086f3cedd53bf1
Anshul Ahuja <anshul.ahu@gmail.com>
2020-01-27 15:01:19 +0530

    Remove redundant describe from userpanel specs

--- cypress/integration/specs/userpanel.spec.ts
+++ cypress/integration/specs/userpanel.spec.ts
@@ -5,33 +5,31 @@ describe('Userpanel', () => {
     UserpanelPage.visitHome();
   });
 
-  describe('Basic tests', () => {
-    it('check default namespace', () => {
-      UserpanelPage.assertUrlContains('overview?namespace=default');
-    });
+  it('check default namespace', () => {
+    UserpanelPage.assertUrlContains('overview?namespace=default');
+  });
 
-    it('collapses sidebar', () => {
-      UserpanelPage.assertVisibility('mat-drawer', true);
-      UserpanelPage.clickItem('kd-nav-hamburger');
-      UserpanelPage.assertVisibility('mat-drawer', false);
-    });
+  it('collapses sidebar', () => {
+    UserpanelPage.assertVisibility('mat-drawer', true);
+    UserpanelPage.clickItem('kd-nav-hamburger');
+    UserpanelPage.assertVisibility('mat-drawer', false);
+  });
 
-    it('home logo click overview redirect check', () => {
-      UserpanelPage.clickItem('.kd-toolbar-logo-link');
-      UserpanelPage.assertUrlContains('overview');
-    });
+  it('home logo click overview redirect check', () => {
+    UserpanelPage.clickItem('.kd-toolbar-logo-link');
+    UserpanelPage.assertUrlContains('overview');
+  });
 
-    it('add resource', () => {
-      UserpanelPage.clickItem('mat-icon.kd-primary-toolbar-icon', 'add');
-      UserpanelPage.assertUrlContains('create');
-    });
+  it('add resource', () => {
+    UserpanelPage.clickItem('mat-icon.kd-primary-toolbar-icon', 'add');
+    UserpanelPage.assertUrlContains('create');
+  });
 
-    it('search', () => {
-      const searchString = 'test_string';
-      UserpanelPage.visitHome();
-      UserpanelPage.typeSearch(searchString);
-      UserpanelPage.assertUrlContains('q=' + searchString);
-      UserpanelPage.visitHome();
-    });
+  it('search', () => {
+    const searchString = 'test_string';
+    UserpanelPage.visitHome();
+    UserpanelPage.typeSearch(searchString);
+    UserpanelPage.assertUrlContains('q=' + searchString);
+    UserpanelPage.visitHome();
   });
 });
