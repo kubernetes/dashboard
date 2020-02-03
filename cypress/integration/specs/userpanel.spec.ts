0434a94731719b91e183c5eac9a20094f819ca47
Anshul Ahuja <anshul.ahu@gmail.com>
2020-02-03 16:55:21 +0530

    Add cypress tests (navbar, userpanel) (#4435)
    * Create webpack.config.js
    
    * Add ts based cypress tests for nav links
    
    * Cleanup commands.ts
    
    * Add ids to nav/template.html elements
    
    * Refactor & cleanup methods
    
    * Add dependancies
    
    * Fix no-any tslint
    
    * Fix linting
    
    * Add test for all other nav items
    
    * Create abstractPage to inherit in other pages, refactor code
    
    * Refactor methods
    
    * Refactor AbstractPage methods
    
    * Add userpanel tests
    
    * Minor fixes, linting
    
    * Move navbar item IDs to NavbarPage
    
    * Add new lines between tests to enhance readability
    
    * Remove redundant 'navbar' from nav spec describe
    
    * Remove redundant describe from userpanel specs
    
    * Move to devdependancy
    
    * manually update package jsons
    
    * Update package-lock.json
    
    * Add ts-loader dependancy
    
    * Add missing ,

---
+++ cypress/integration/specs/userpanel.spec.ts
@@ -0,0 +1,35 @@
+import {UserpanelPage} from '../../pages/userpanelPage';
+
+describe('Userpanel', () => {
+  before(() => {
+    UserpanelPage.visitHome();
+  });
+
+  it('check default namespace', () => {
+    UserpanelPage.assertUrlContains('overview?namespace=default');
+  });
+
+  it('collapses sidebar', () => {
+    UserpanelPage.assertVisibility('mat-drawer', true);
+    UserpanelPage.clickItem('kd-nav-hamburger');
+    UserpanelPage.assertVisibility('mat-drawer', false);
+  });
+
+  it('home logo click overview redirect check', () => {
+    UserpanelPage.clickItem('.kd-toolbar-logo-link');
+    UserpanelPage.assertUrlContains('overview');
+  });
+
+  it('add resource', () => {
+    UserpanelPage.clickItem('mat-icon.kd-primary-toolbar-icon', 'add');
+    UserpanelPage.assertUrlContains('create');
+  });
+
+  it('search', () => {
+    const searchString = 'test_string';
+    UserpanelPage.visitHome();
+    UserpanelPage.typeSearch(searchString);
+    UserpanelPage.assertUrlContains('q=' + searchString);
+    UserpanelPage.visitHome();
+  });
+});
--- cypress/support/README.md
+++
@@ -1,5 +0,0 @@
-The `support/index.js` is processed and
-loaded automatically before the test files.
-
-This is the place to put global configuration and
-behavior that modifies Cypress.
