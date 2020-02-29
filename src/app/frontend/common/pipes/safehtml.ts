5a756b1875a36bf84de098061d6409b8f8f1a366
Gregg Jensen <greggbjensen@users.noreply.github.com>
2020-02-29 02:30:39 -0700

    Added support for ANSI color codes to logged terminal output. (#4963)
    * Added support for ANSI color codes to logged terminal output.
    
    * package-lock.json fixes, by reverting to original versions.
    
    * Added enum and switch statement for colored output.  Fixed ansi-to-html module version.
    
    * Updated package-lock.json to keep in sync.

--- src/app/frontend/common/pipes/safehtml.ts
+++ src/app/frontend/common/pipes/safehtml.ts
@@ -15,6 +15,14 @@
 import {Pipe, SecurityContext} from '@angular/core';
 import {DomSanitizer, SafeHtml} from '@angular/platform-browser';
 
+const ansiColorClass = require('ansi-to-html');
+const ansiColor = new ansiColorClass();
+
+enum TextMode {
+  Default = 'Default',
+  Colored = 'Colored',
+}
+
 /**
  * Formats the given value as raw HTML to display to the user.
  */
@@ -22,10 +30,26 @@ import {DomSanitizer, SafeHtml} from '@angular/platform-browser';
 export class SafeHtmlFormatter {
   constructor(private readonly sanitizer: DomSanitizer) {}
 
-  transform(value: string): SafeHtml {
-    return this.sanitizer.sanitize(
+  transform(value: string, mode: TextMode = TextMode.Default): SafeHtml {
+    let result: SafeHtml = null;
+    let content = this.sanitizer.sanitize(
       SecurityContext.HTML,
       value.replace('<', '&lt;').replace('>', '&gt;'),
     );
+
+    // Handle conversion of ANSI color codes.
+    switch (mode) {
+      case TextMode.Colored:
+        content = ansiColor.toHtml(content.replace(/&#27;/g, '\u001b'));
+        result = this.sanitizer.bypassSecurityTrustHtml(content);
+        break;
+
+      default:
+        // TextMode.Default
+        result = content;
+        break;
+    }
+
+    return result;
   }
 }
