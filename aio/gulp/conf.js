cc1b9be0f832c8726f2d05f37dff74489ed1b0ce
Sebastian Florek <sebastian.florek@loodse.com>
2020-03-30 13:07:27 +0200

    Release v2.0.0-rc7

--- aio/gulp/conf.js
+++ aio/gulp/conf.js
@@ -53,7 +53,7 @@ const version = {
   /**
    * Current release version of the project.
    */
-  release: 'v2.0.0-rc6',
+  release: 'v2.0.0-rc7',
   /**
    * Version name of the head release of the project.
    */
