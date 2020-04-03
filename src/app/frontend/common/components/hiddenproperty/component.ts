cb2c4cce33048c0b85304981d017f2b5af5e55fb
Gregg B. Jensen <greggbjensen@users.noreply.github.com>
2020-04-03 07:59:46 -0600

    Inline edit secret enhancements (#5046)
    * Completed toggling between visible read-only and edit for inline secret.
    
    * Changed inline edit button for secret to blue with border.
    
    * Added auto-size textarea for multi-line and input for single line secret editing.
    
    * Ran fontend fixes.
    
    * Added enableEdit flag to HiddenPropertyComponent.
    
    * Improve styling and behavior
    
    Co-authored-by: Sebastian Florek <sebastian.florek@loodse.com>

--- src/app/frontend/common/components/hiddenproperty/component.ts
+++ src/app/frontend/common/components/hiddenproperty/component.ts
@@ -14,11 +14,32 @@
 
 import {Component, Input} from '@angular/core';
 
+export enum HiddenPropertyMode {
+  Hidden = 'hidden',
+  Visible = 'visible',
+  Edit = 'edit',
+}
+
 @Component({
   selector: 'kd-hidden-property',
   templateUrl: './template.html',
   styleUrls: ['./style.scss'],
 })
 export class HiddenPropertyComponent {
-  @Input() hidden = true;
+  @Input() mode = HiddenPropertyMode.Hidden;
+  @Input() enableEdit = false;
+
+  HiddenPropertyMode = HiddenPropertyMode;
+
+  toggleVisible(): void {
+    this.mode =
+      this.mode !== HiddenPropertyMode.Visible
+        ? HiddenPropertyMode.Visible
+        : HiddenPropertyMode.Hidden;
+  }
+
+  toggleEdit(): void {
+    this.mode =
+      this.mode !== HiddenPropertyMode.Edit ? HiddenPropertyMode.Edit : HiddenPropertyMode.Hidden;
+  }
 }
