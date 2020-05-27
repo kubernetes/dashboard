01f53b245def490304c9a6a0e01b45831221703e
Sebastian Florek <Sebastian.Florek@loodse.com>
2020-05-27 15:04:41 +0200

    Fix can-i check for settings configmap (#5182)

--- src/app/backend/client/api/common.go
+++ src/app/backend/client/api/common.go
@@ -16,19 +16,23 @@ package api
 
 import (
 	"crypto/rand"
+	"fmt"
+	"strings"
 
 	v1 "k8s.io/api/authorization/v1"
 )
 
 // ToSelfSubjectAccessReview creates kubernetes API object based on provided data.
-func ToSelfSubjectAccessReview(namespace, name, resource, verb string) *v1.SelfSubjectAccessReview {
+func ToSelfSubjectAccessReview(namespace, name, resourceKind, verb string) *v1.SelfSubjectAccessReview {
 	return &v1.SelfSubjectAccessReview{
 		Spec: v1.SelfSubjectAccessReviewSpec{
 			ResourceAttributes: &v1.ResourceAttributes{
 				Namespace: namespace,
 				Name:      name,
-				Resource:  resource,
-				Verb:      verb,
+				// Resource kind name must be in a plural lower-case format,
+				Resource: fmt.Sprintf("%ss", strings.ToLower(resourceKind)),
+				// Let's enforce the same lower-case format for verbs
+				Verb: strings.ToLower(verb),
 			},
 		},
 	}
