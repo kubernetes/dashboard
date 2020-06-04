619adc975457ebfdcbfa8a8f04343859a1e7aa11
Marcin Maciaszczyk <maciaszczykm@icloud.com>
2020-06-04 14:33:13 +0200

    Format files

--- .prettierrc.js
+++ .prettierrc.js
@@ -5,13 +5,10 @@ const modifiedConfig = _.merge(
   {},
   gtsConfig,
   {
-    arrowParens: 'avoid',
-    bracketSpacing: false,
     insertPragma: false,
     printWidth: 120,
     requirePragma: false,
     semi: true,
-    singleQuote: true,
     tabWidth: 2,
     trailingComma: 'all',
     useTabs: false,
