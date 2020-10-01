078741232c24d5fe993f42e7d1073d78bfa85637
Marcin Maciaszczyk <maciaszczykm@icloud.com>
2020-10-01 13:27:39 +0200

    Update configmap route

--- pkg/api/server.go
+++ pkg/api/server.go
@@ -15,65 +15,67 @@
 package api
 
 import (
-  "fmt"
-  "net"
+	"fmt"
+	"github.com/kubernetes/dashboard/pkg/api/v1/configmap"
+	"net"
 
-  "google.golang.org/grpc"
-  "google.golang.org/grpc/reflection"
-  "k8s.io/klog"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+	"k8s.io/klog"
 
-  "github.com/kubernetes/dashboard/pkg/api/middleware"
-  v1 "github.com/kubernetes/dashboard/pkg/api/v1"
-  "github.com/kubernetes/dashboard/pkg/api/v1/deployment"
-  "github.com/kubernetes/dashboard/pkg/api/v1/pod"
-  "github.com/kubernetes/dashboard/pkg/cmd/dashboard/options"
+	"github.com/kubernetes/dashboard/pkg/api/middleware"
+	v1 "github.com/kubernetes/dashboard/pkg/api/v1"
+	"github.com/kubernetes/dashboard/pkg/api/v1/deployment"
+	"github.com/kubernetes/dashboard/pkg/api/v1/pod"
+	"github.com/kubernetes/dashboard/pkg/cmd/dashboard/options"
 )
 
 // VERSION of this binary
 var Version = "DEV"
 
 type Server struct {
-  options *options.Options
-  grpc    *grpc.Server
+	options *options.Options
+	grpc    *grpc.Server
 }
 
 func (s *Server) Run() error {
-  klog.Infof("starting Kubernetes Dashboard API Server: %+v", Version)
+	klog.Infof("starting Kubernetes Dashboard API Server: %+v", Version)
 
-  // TODO: Handle HTTPS connection
-  listener, err := net.Listen("tcp", fmt.Sprintf("%s:%d", s.options.InsecureBindAddress, s.options.InsecurePort))
-  if err != nil {
-    klog.Fatalf("failed to listen: %v", err)
-  }
+	// TODO: Handle HTTPS connection
+	listener, err := net.Listen("tcp", fmt.Sprintf("%s:%d", s.options.InsecureBindAddress, s.options.InsecurePort))
+	if err != nil {
+		klog.Fatalf("failed to listen: %v", err)
+	}
 
-  klog.Infof("listening on %s:%d", s.options.InsecureBindAddress, s.options.InsecurePort)
+	klog.Infof("listening on %s:%d", s.options.InsecureBindAddress, s.options.InsecurePort)
 
-  s.init()
-  return s.grpc.Serve(listener)
+	s.init()
+	return s.grpc.Serve(listener)
 }
 
 func (s *Server) init() {
-  unaryInterceptors := middleware.NewUnaryInterceptorBuilder().Add(middleware.InterceptorTypeAuth).AsOptions()
-  streamInterceptors := middleware.NewStreamInterceptorBuilder().Add(middleware.InterceptorTypeAuth).AsOptions()
+	unaryInterceptors := middleware.NewUnaryInterceptorBuilder().Add(middleware.InterceptorTypeAuth).AsOptions()
+	streamInterceptors := middleware.NewStreamInterceptorBuilder().Add(middleware.InterceptorTypeAuth).AsOptions()
 
-  s.grpc = grpc.NewServer(append(unaryInterceptors, streamInterceptors...)...)
+	s.grpc = grpc.NewServer(append(unaryInterceptors, streamInterceptors...)...)
 
-  s.register(
-    pod.NewPodRouteHandler(),
-    deployment.NewDeploymentRouteHandler(),
-  )
+	s.register(
+		configmap.NewConfigMapRouteHandler(),
+		pod.NewPodRouteHandler(),
+		deployment.NewDeploymentRouteHandler(),
+	)
 
-  // Enable reflection so we can find available routes
-  // TODO: Disable reflection once API is stable
-  reflection.Register(s.grpc)
+	// Enable reflection so we can find available routes
+	// TODO: Disable reflection once API is stable
+	reflection.Register(s.grpc)
 }
 
 func (s *Server) register(routes ...v1.RouteHandler) {
-  for _, route := range routes {
-    route.Install(s.grpc)
-  }
+	for _, route := range routes {
+		route.Install(s.grpc)
+	}
 }
 
 func NewServer(options *options.Options) *Server {
-  return &Server{options: options}
+	return &Server{options: options}
 }
