22aa031b330b53fd10ae2abac1ca08c6f4add97a
Marcin Maciaszczyk <maciaszczykm@icloud.com>
2020-10-15 13:30:16 +0200

    Update dependencies and remove tslib leftovers

--- src/app/frontend/index.spec.ts
+++ src/app/frontend/index.spec.ts
@@ -13,7 +13,6 @@
 // limitations under the License.
 
 // This file is required by karma.conf.js and loads recursively all the .spec and framework files.
-/* tslint:disable */
 // import 'zone.js/dist/zone.js';
 import 'zone.js/dist/async-test';
 import 'zone.js/dist/fake-async-test';
@@ -21,16 +20,13 @@ import 'zone.js/dist/long-stack-trace-zone';
 import 'zone.js/dist/proxy';
 import 'zone.js/dist/sync-test';
 import 'zone.js/dist/jasmine-patch';
-/* tslint:enable */
 
 import {getTestBed} from '@angular/core/testing';
 import {BrowserDynamicTestingModule, platformBrowserDynamicTesting} from '@angular/platform-browser-dynamic/testing';
 
 // Unfortunately there's no typing for the `__karma__` variable. Just declare it as any.
-/* tslint:disable */
 declare const __karma__: any;
 declare const require: any;
-/* tslint:enable */
 
 // Prevent Karma from running prematurely.
 __karma__.loaded = () => {};
