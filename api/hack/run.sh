f9f4e7bede38a9e15884fff1c53e3a3c39af5236
Sebastian Florek <sebastian.florek@kubermatic.com>
2020-10-29 13:11:22 +0100

    Update config

--- api/hack/run.sh
+++ api/hack/run.sh
@@ -6,5 +6,5 @@ KUBECONFIG="${KUBECONFIG:-${HOME}/.kube/config}"
 ENABLE_REFLECTION_API="${REFLECTION_API:-false}"
 
 go run github.com/kubernetes/dashboard/pkg/cmd \
-  --kubeconfig=${KUBECONFIG} \
-  --enable-reflection-api=${ENABLE_REFLECTION_API}
+  --kubeconfig="${KUBECONFIG}" \
+  --enable-reflection-api="${ENABLE_REFLECTION_API}"
