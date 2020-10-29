583552f46e698079f8593f749db7eee5f003248c
Marcin Maciaszczyk <maciaszczykm@icloud.com>
2020-10-29 13:10:30 +0100

    Update run.sh

--- api/hack/run.sh
+++ api/hack/run.sh
@@ -2,9 +2,9 @@
 
 set -euo pipefail
 
-KUBECONFIG="${KUBECONFIG:-${HOME}/.kube/config}"
-ENABLE_REFLECTION_API="${REFLECTION_API:-false}"
-
 go run github.com/kubernetes/dashboard/pkg/cmd \
-  --kubeconfig=${KUBECONFIG} \
-  --enable-reflection-api=${ENABLE_REFLECTION_API}
+  --kubeconfig=${KUBECONFIG:-${HOME}/.kube/config} \
+  --port=${PORT:-8080} \
+  --bind-address=${BIND_ADDRESS:-127.0.0.1} \
+  --sidecar-host=${SIDECAR_HOST:-http://localhost:8000} \
+  --enable-reflection-api=${ENABLE_REFLECTION_API:-false}
