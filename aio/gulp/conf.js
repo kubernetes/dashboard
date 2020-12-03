8b89a1c7b670ddd8685c9eb5892dd54d2de625ab
Marcin Maciaszczyk <maciaszczykm@icloud.com>
2020-12-03 14:20:14 +0100

    Bump Dashboard version to 2.0.5 (#5702)
    * Bump Dashboard version to 2.0.5
    
    * Revert chart change

--- aio/gulp/conf.js
+++ aio/gulp/conf.js
@@ -53,7 +53,7 @@ const version = {
   /**
    * Current release version of the project.
    */
-  release: 'v2.0.4',
+  release: 'v2.0.5',
   /**
    * Version name of the head release of the project.
    */
