2b17505ee075468c677d33f1d6355da5a10188b0
Sebastian Florek <s.florek91@gmail.com>
2021-03-25 12:49:31 +0100

    Refactor cypress config and improve scale request (#5928)
    * Add dependabot, refactor cypress config, improve scale request
    
    * Fix husky ignore
    
    * Fix config
    
    * i18n

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,8 +1,3 @@
-const wp = require('@cypress/webpack-preprocessor')
+const cypressTypeScriptPreprocessor = require('./cy-ts-preprocessor');
 
-module.exports = (on) => {
-  const options = {
-    webpackOptions: require('../../webpack.config'),
-  }
-  on('file:preprocessor', wp(options))
-}
+module.exports = on => on('file:preprocessor', cypressTypeScriptPreprocessor);
