e4ce8bca145500c0f0a63eda57180c1728433e2c
Marcos Diez <marcos@unitron.com.br>
2021-07-01 09:34:30 -0300

    Kind and Object row

--- src/app/frontend/common/components/resourcelist/event/component.ts
+++ src/app/frontend/common/components/resourcelist/event/component.ts
@@ -71,7 +71,7 @@ export class EventListComponent extends ResourceListWithStatuses<EventList, Even
   }
 
   getDisplayColumns(): string[] {
-    return ['statusicon', 'message', 'source', 'subobject', 'count', 'firstseen', 'lastseen'];
+    return ['statusicon', 'type', 'message', 'source', 'object', 'count', 'firstseen', 'lastseen'];
   }
 
   getObjectHref(kind: string, name: string, namespace: string): string {
