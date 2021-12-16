d64a8a593632f9f85b61fec05ee97b3f1ccb8af2
Marcin Maciaszczyk <marcin9yk@icloud.com>
2021-12-16 14:45:27 +0100

    Remove unused import

--- src/app/frontend/common/services/global/plugin.ts
+++ src/app/frontend/common/services/global/plugin.ts
@@ -16,7 +16,6 @@ import {HttpClient} from '@angular/common/http';
 import {Injectable} from '@angular/core';
 import {PluginMetadata, PluginsConfig} from '@api/root.ui';
 import {Observable} from 'rxjs';
-import {tap} from 'rxjs/operators';
 
 @Injectable()
 export class PluginsConfigService {
