86b8372e933e3066519ba2bcb45426bbde6c23fc
Shu Muto <shu.mutow@nec.com>
2021-12-22 17:29:36 +0900

    Bump k8s modules to 0.23.1 (#6672)

--- src/app/backend/plugin/client/clientset/versioned/typed/apis/v1alpha1/apis_client.go
+++ src/app/backend/plugin/client/clientset/versioned/typed/apis/v1alpha1/apis_client.go
@@ -17,6 +17,8 @@
 package v1alpha1
 
 import (
+	"net/http"
+
 	v1alpha1 "github.com/kubernetes/dashboard/src/app/backend/plugin/apis/v1alpha1"
 	"github.com/kubernetes/dashboard/src/app/backend/plugin/client/clientset/versioned/scheme"
 	rest "k8s.io/client-go/rest"
@@ -37,12 +39,28 @@ func (c *DashboardV1alpha1Client) Plugins(namespace string) PluginInterface {
 }
 
 // NewForConfig creates a new DashboardV1alpha1Client for the given config.
+// NewForConfig is equivalent to NewForConfigAndClient(c, httpClient),
+// where httpClient was generated with rest.HTTPClientFor(c).
 func NewForConfig(c *rest.Config) (*DashboardV1alpha1Client, error) {
 	config := *c
 	if err := setConfigDefaults(&config); err != nil {
 		return nil, err
 	}
-	client, err := rest.RESTClientFor(&config)
+	httpClient, err := rest.HTTPClientFor(&config)
+	if err != nil {
+		return nil, err
+	}
+	return NewForConfigAndClient(&config, httpClient)
+}
+
+// NewForConfigAndClient creates a new DashboardV1alpha1Client for the given config and http client.
+// Note the http client provided takes precedence over the configured transport values.
+func NewForConfigAndClient(c *rest.Config, h *http.Client) (*DashboardV1alpha1Client, error) {
+	config := *c
+	if err := setConfigDefaults(&config); err != nil {
+		return nil, err
+	}
+	client, err := rest.RESTClientForConfigAndClient(&config, h)
 	if err != nil {
 		return nil, err
 	}
