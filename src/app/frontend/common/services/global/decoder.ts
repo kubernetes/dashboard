b0f3d02e24eddd0ea9ddd5c8c5a3f8167351036d
Sebastian Florek <s.florek91@gmail.com>
2022-02-03 13:13:45 +0100

    Update secret data decode (#6761)

--- src/app/frontend/common/services/global/decoder.ts
+++ src/app/frontend/common/services/global/decoder.ts
@@ -17,13 +17,16 @@ import {Injectable} from '@angular/core';
 @Injectable({providedIn: 'root'})
 export class DecoderService {
   base64(encoded: string): string {
-    return decodeURIComponent(
-      atob(encoded)
-        .split('')
-        .map(c => {
-          return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
-        })
-        .join('')
-    );
+    try {
+      return decodeURIComponent(
+        window
+          .atob(encoded)
+          .split('')
+          .map((c: string) => '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2))
+          .join('')
+      );
+    } catch (e) {
+      return 'Unable to decode file. It might contain binary data.';
+    }
   }
 }
