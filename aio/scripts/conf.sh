c94eb07ad29532bbf838f0a4732b6a8a885034ea
Shu Muto <shu.mutow@nec.com>
2022-02-18 18:14:23 +0900

    Bump k8s modules to 0.24.4 (#6831)
    Also, remove `go get vendor` from `postinstall`.
    Because the existence of `vendor` directory causes error
    on `make fix-codegen` when update codegen version.

--- aio/scripts/conf.sh
+++ aio/scripts/conf.sh
@@ -40,7 +40,7 @@ HEAPSTER_PORT=8082
 KIND_VERSION="v0.11.1"
 K8S_VERSION="v1.21.1"
 KIND_BIN=${CACHE_DIR}/kind-${KIND_VERSION}
-CODEGEN_VERSION="v0.23.3"
+CODEGEN_VERSION="v0.23.4"
 CODEGEN_BIN=${GOPATH}/pkg/mod/k8s.io/code-generator@${CODEGEN_VERSION}/generate-groups.sh
 
 # Setup logger.
