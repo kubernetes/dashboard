94b9dec6061590a3d1e8341e3361c4c44c695701
Shu Muto <shu.mutow@nec.com>
2022-12-18 15:19:28 +0900

    Setup default behavior that exec `make run`

--- hack/develop/npm-command.sh
+++ hack/develop/npm-command.sh
@@ -13,20 +13,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Install dependencies
-echo "Install dependencies"
-npm ci
-
 # Run npm command if K8S_DASHBOARD_NPM_CMD is set,
 # otherwise start dashboard.
-if [[ -n "${K8S_DASHBOARD_NPM_CMD}" ]] ; then
-  # Run npm command
-  echo "Run npm '${K8S_DASHBOARD_NPM_CMD}'"
-  npm ${K8S_DASHBOARD_NPM_CMD} \
-    --bind_address=${K8S_DASHBOARD_BIND_ADDRESS} \
-    --sidecar_host=${K8S_DASHBOARD_SIDECAR_HOST} \
-    --port=${K8S_DASHBOARD_PORT}
-else
+if [[ -v "${K8S_DASHBOARD_NPM_CMD}" ]] ; then
   if [[ "${K8S_OWN_CLUSTER}" != true ]] ; then
     # Stop cluster.
     echo "Stop cluster"
@@ -49,7 +38,7 @@ else
     echo "Kill kubectl ${KUBECTL_PID}"
     kill ${KUBECTL_PID}
     nohup kubectl proxy --address 127.0.0.1 --port 8000 >/tmp/kubeproxy.log 2>&1 &
-    export K8S_DASHBOARD_SIDECAR_HOST="http://localhost:8000/api/v1/namespaces/kubernetes-dashboard/services/dashboard-metrics-scraper:/proxy/"
+    export SIDECAR_HOST="http://localhost:8000/api/v1/namespaces/kubernetes-dashboard/services/dashboard-metrics-scraper:/proxy/"
     # Inform how to get token for logging in to dashboard
     echo "HOW TO GET TOKEN FOR LOGGING INTO DASHBOARD"
     echo "1. Run terminal for dashboard container."
@@ -57,10 +46,8 @@ else
     echo "2. Run following to get token for logging into dashboard."
     echo "  kubectl -n kubernetes-dashboard create token kubernetes-dashboard"
   fi
-  # Start dashboard.
-  echo "Start dashboard in production mode"
-  npm run start:prod \
-    --bind_address=${K8S_DASHBOARD_BIND_ADDRESS} \
-    --sidecar_host=${K8S_DASHBOARD_SIDECAR_HOST} \
-    --port=${K8S_DASHBOARD_PORT}
 fi
+
+# Start dashboard.
+echo "Start dashboard in production mode"
+make run
