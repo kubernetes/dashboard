0c95291343927b51a9ffd72150ed8fb123170d4f
Shu Muto <shu.mutow@nec.com>
2023-03-16 11:05:28 +0000

    Bump k8s version for kind to 1.26.2

--- hack/scripts/conf.sh
+++ hack/scripts/conf.sh
@@ -29,7 +29,7 @@ ARCH=$(uname | awk '{print tolower($0)}')
 HEAPSTER_VERSION="v1.5.4"
 HEAPSTER_PORT=8082
 KIND_VERSION="v0.17.0"
-K8S_VERSION="v1.26.0"
+K8S_VERSION="v1.26.2"
 KIND_BIN=${CACHE_DIR}/kind-${KIND_VERSION}
 
 function ensure-cache {
