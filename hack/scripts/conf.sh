fdd1ae8bc35eef0ef1145deed66fa4fc9e14f6a0
Shu Muto <shu.mutow@nec.com>
2023-06-02 18:04:53 +0900

    Bump kind version to 0.19.0 and node image to 1.27.2. (#7755)

--- hack/scripts/conf.sh
+++ hack/scripts/conf.sh
@@ -28,8 +28,8 @@ ARCH=$(uname | awk '{print tolower($0)}')
 # Local cluster configuration (check start-cluster.sh script for more details).
 HEAPSTER_VERSION="v1.5.4"
 HEAPSTER_PORT=8082
-KIND_VERSION="v0.17.0"
-K8S_VERSION="v1.27.0"
+KIND_VERSION="v0.19.0"
+K8S_VERSION="v1.27.2"
 KIND_BIN=${CACHE_DIR}/kind-${KIND_VERSION}
 
 function ensure-cache {
