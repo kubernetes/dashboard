584f975fe8ab8fe8da3a33ca24c4807bb210bcfd
Marcin Maciaszczyk <marcin9yk@icloud.com>
2023-06-23 15:19:25 +0200

    Add missing config file

---
+++ modules/metrics-scraper/hack/include/config.mk
@@ -0,0 +1,8 @@
+### Application details
+APP_NAME := $(PROJECT_NAME)-metrics-scraper
+APP_VERSION := v1.0.9
+PACKAGE_NAME := k8s.io/$(PROJECT_NAME)/metrics-scraper
+
+### Dirs and paths
+COVERAGE_FILE = $(METRICS_SCRAPER_DIRECTORY)/coverage.out
+
