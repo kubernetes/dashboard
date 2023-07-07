80b0c03ea55f33a776566ef10d7c879c94546cc6
Marcin Maciaszczyk <marcin9yk@icloud.com>
2023-07-07 12:28:39 +0200

    Build

--- modules/metrics-scraper/hack/include/config.mk
+++ modules/metrics-scraper/hack/include/config.mk
@@ -4,5 +4,6 @@ APP_VERSION := v1.0.9
 PACKAGE_NAME := k8s.io/$(PROJECT_NAME)/metrics-scraper
 
 ### Dirs and paths
+METRICS_SCRAPER_DIST_DIRECTORY = $(DIST_DIRECTORY)/metrics-scraper
 COVERAGE_FILE = $(METRICS_SCRAPER_DIRECTORY)/coverage.out
 
