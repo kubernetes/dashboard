5791d0c20e5735d0ee27aa26ddcd2ca974995d57
Sebastian Florek <s.florek91@gmail.com>
2023-08-18 14:52:06 +0200

    update cypress configuration

--- modules/web/cypress/plugins/index.ts
+++ modules/web/cypress/plugins/index.ts
@@ -14,15 +14,10 @@
  * limitations under the License.
  *
  */
-import webpack from '@cypress/webpack-preprocessor';
 import failFast from 'cypress-fail-fast/plugin';
 import del from 'del';
-import {configuration} from './cy-ts-preprocessor';
 
-// @ts-ignore
 export default async (on, config) => {
-  on('file:preprocessor', webpack(configuration));
-
   // Remove videos of successful tests and keep only failed ones.
   // @ts-ignore
   on('after:spec', (_, results) => {
