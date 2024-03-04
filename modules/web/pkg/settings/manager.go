a2584507a4f8dfe77a0896790254acc191a17073
Sebastian Florek <sebastian@plural.sh>
2024-03-04 12:41:56 +0100

    make fix

--- modules/web/pkg/settings/manager.go
+++ modules/web/pkg/settings/manager.go
@@ -81,11 +81,7 @@ func (sm *SettingsManager) load(client kubernetes.Interface) (changed bool) {
 	configMap, err := client.CoreV1().ConfigMaps(args.Namespace()).
 		Get(context.Background(), args.SettingsConfigMapName(), metav1.GetOptions{})
 	if err != nil {
-		klog.Errorf("Cannot find settings config map: %s", err.Error())
-		//err = sm.restoreConfigMap(client)
-		//if err != nil {
-		//	log.Printf("Cannot restore settings config map: %s", err.Error())
-		//}
+		klog.V(1).Infof("Could not get settings config map: %s", err.Error())
 		return
 	}
 
@@ -115,29 +111,6 @@ func (sm *SettingsManager) load(client kubernetes.Interface) (changed bool) {
 	}
 
 	return
-}
-
-func (sm *SettingsManager) restoreConfigMap(client kubernetes.Interface) error {
-	configMap := &v1.ConfigMap{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      args.SettingsConfigMapName(),
-			Namespace: args.Namespace(),
-		},
-		Data: map[string]string{
-			ConfigMapSettingsKey: defaultSettings.Marshal(),
-		},
-	}
-
-	restoredConfigMap, err := client.CoreV1().ConfigMaps(args.Namespace()).
-		Create(context.Background(), configMap, metav1.CreateOptions{})
-	if err != nil {
-		return err
-	}
-
-	sm.settings = &defaultSettings
-	sm.rawSettings = restoredConfigMap.Data
-
-	return nil
 }
 
 func (sm *SettingsManager) GetGlobalSettings(client kubernetes.Interface) *Settings {
