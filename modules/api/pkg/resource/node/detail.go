7825554e4525d1dd1650946c90324a81428ee26d
Marcin Maciaszczyk <marcin9yk@icloud.com>
2024-05-08 13:46:37 +0200

    add pod allocated resources (#9018)

--- modules/api/pkg/resource/node/detail.go
+++ modules/api/pkg/resource/node/detail.go
@@ -172,8 +172,8 @@ func GetNodeDetail(client k8sClient.Interface, metricClient metricapi.MetricClie
 func getNodeAllocatedResources(node v1.Node, podList *v1.PodList) (NodeAllocatedResources, error) {
 	reqs, limits := map[v1.ResourceName]resource.Quantity{}, map[v1.ResourceName]resource.Quantity{}
 
-	for _, pod := range podList.Items {
-		podReqs, podLimits, err := PodRequestsAndLimits(&pod)
+	for _, p := range podList.Items {
+		podReqs, podLimits, err := pod.PodRequestsAndLimits(&p)
 		if err != nil {
 			return NodeAllocatedResources{}, err
 		}
@@ -231,63 +231,6 @@ func getNodeAllocatedResources(node v1.Node, podList *v1.PodList) (NodeAllocated
 		PodCapacity:            podCapacity,
 		PodFraction:            podFraction,
 	}, nil
-}
-
-// PodRequestsAndLimits returns a dictionary of all defined resources summed up for all
-// containers of the pod. If pod overhead is non-nil, the pod overhead is added to the
-// total container resource requests and to the total container limits which have a
-// non-zero quantity.
-func PodRequestsAndLimits(pod *v1.Pod) (reqs, limits v1.ResourceList, err error) {
-	reqs, limits = v1.ResourceList{}, v1.ResourceList{}
-	for _, container := range pod.Spec.Containers {
-		addResourceList(reqs, container.Resources.Requests)
-		addResourceList(limits, container.Resources.Limits)
-	}
-	// init containers define the minimum of any resource
-	for _, container := range pod.Spec.InitContainers {
-		maxResourceList(reqs, container.Resources.Requests)
-		maxResourceList(limits, container.Resources.Limits)
-	}
-
-	// Add overhead for running a pod to the sum of requests and to non-zero limits:
-	if pod.Spec.Overhead != nil {
-		addResourceList(reqs, pod.Spec.Overhead)
-
-		for name, quantity := range pod.Spec.Overhead {
-			if value, ok := limits[name]; ok && !value.IsZero() {
-				value.Add(quantity)
-				limits[name] = value
-			}
-		}
-	}
-	return
-}
-
-// addResourceList adds the resources in newList to list
-func addResourceList(list, new v1.ResourceList) {
-	for name, quantity := range new {
-		if value, ok := list[name]; !ok {
-			list[name] = quantity.DeepCopy()
-		} else {
-			value.Add(quantity)
-			list[name] = value
-		}
-	}
-}
-
-// maxResourceList sets list to the greater of list/newList for every resource
-// either list
-func maxResourceList(list, new v1.ResourceList) {
-	for name, quantity := range new {
-		if value, ok := list[name]; !ok {
-			list[name] = quantity.DeepCopy()
-			continue
-		} else {
-			if quantity.Cmp(value) > 0 {
-				list[name] = quantity.DeepCopy()
-			}
-		}
-	}
 }
 
 // GetNodePods return pods list in given named node
